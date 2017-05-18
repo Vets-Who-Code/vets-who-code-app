f3339785281b2dfaa2445cb1c78fc9c721d200f7
Jerome Hardaway <jerome@vetswhocode.io>
2017-05-18 14:11:27 -0500

    syllabus complete

---
+++ HTML/assets/css/main.js
@@ -0,0 +1,8 @@
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
+
+export default StyleSheet.create({});
