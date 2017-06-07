7a47bec97e20896733d8a3cb60d5bf1c0ebcfc3e
mrbernnz <noel.sagaille@gmail.com>
2017-06-07 09:12:47 -0400

    * Add Express Framework.
      Changes:
    
        [√] :page_facing_up: .gitignore
        [√] :page_facing_up: .nvmrc
        [√] :page_facing_up: app.js
        [√] :page_facing_up: package.json
        [√] :page_facing_up: yarn.lock

---
+++ app.js
@@ -0,0 +1,23 @@
+const debug = require('debug')('app')
+const express = require('express')
+const app = express()
+
+app
+  .set('port', process.env.PORT || 4000)
+  .use(express.static(`${__dirname}/HTML`))
+
+  /*
+   * .use((err, req, res, next) => {
+   *   res.status(404).sendFile(`${__dirname}/HTML/404.html`)
+   * })
+   */
+
+  .use((err, req, res, next) => {
+    res.status(err.status)
+    res.render('Error', {
+      message: err.message,
+      error: (app.get('env') === 'development') ? err : {}
+    })
+  })
+
+  .listen(app.get('port'), () => debug(`Listening on ${app.get('port')}`))
