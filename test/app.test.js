c9d185bc33511beeda479133655e2e60bea1e5f4
mrbernnz <noel.sagaille@gmail.com>
2017-09-21 13:43:36 -0400

    * Add environment value.
      Why:
    
        Necessary for codeship.
    
      Changes:
    
        [√] :wrench: test/app.test.js
        [√] :wrench: README.md

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ require('../app')
 
 describe('VetsWhoCode\'s website', () => {
   it('should respond with a 200 status code', done => {
-    http.get('http://127.0.0.1:3000', response => {
+    http.get(`http://127.0.0.1:${process.env.PORT}`, response => {
       assert.equal(200, response.statusCode)
       done()
     })
