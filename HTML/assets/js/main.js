a66b5486b7869aeb3e919011afb8fab24cc12618
Jerome Hardaway <jerome@vetswhocode.io>
2017-09-25 13:31:16 -0500

    changed date

--- HTML/assets/js/main.js
+++ HTML/assets/js/main.js
@@ -182,7 +182,7 @@ $(document).ready(function(){
     if($(".container-countdown").length == 0) return;
 
     $('.container-countdown').countdown({
-        date: "September 04, 2017 00:00:00",
+        date: "March 05, 2018 00:00:00",
         render: function(data) {
             var el = $(this.el);
             el.empty()
