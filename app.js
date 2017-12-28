243b694503c22cbd49d1866f8ba8828a3dd1af4e
mrbernnz <noel.sagaille@gmail.com>
2017-12-28 13:46:06 -0500

    [Refactor] Directory Structure
    * Rename static file directory.
    
      Changes:
    
        [√] :pencil2: HTML -> public
    
    * Edit path name of assets.
    
      Changes:
    
        [√] :wrench: .idea/workspace.xml
        [√] :wrench: app.js
        [√] :wrench: public/404.html
        [√] :wrench: public/about_us.html
        [√] :wrench: public/apply.html
        [√] :wrench: public/contact.html
        [√] :wrench: public/donate.html
        [√] :wrench: public/index.html
        [√] :wrench: public/media.html
        [√] :wrench: public/mentor.html
        [√] :wrench: public/podcast.html
        [√] :wrench: public/services.html
        [√] :wrench: public/syllabus.html
    
    * Ignore Visual Studio Code settings.
    
      Why:
    
        That should be specific to developer's machine settings.
    
      Changes:
    
        [√] :wrench: .gitignore

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const debug = require('debug')('app')
 const express = require('express')
 const app = express()
 
 app
   .set('port', process.env.PORT || 4000)
-  .use(express.static('HTML'))
-
+  .use(express.static('public'))
   /*
    * .use((err, req, res, next) => {
    *   res.status(404).sendFile(`${__dirname}/HTML/404.html`)
@@ -16,8 +14,7 @@ app
     res.status(err.status)
     res.render('Error', {
       message: err.message,
-      error: (app.get('env') === 'development') ? err : {}
+      error: app.get('env') === 'development' ? err : {}
     })
   })
-
-  .listen(app.get('port'), () => debug(`Listening on ${app.get('port')}`))
+  .listen(app.get('port'), () => console.log(`Listening on ${app.get('port')}`))
