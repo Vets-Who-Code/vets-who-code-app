5459ad6de119237065513c0c71a784dbc105888f
Andrew James <ajames20@gmail.com>
2018-01-11 10:33:22 -0700

    add appyly page

---
+++ vwc-gatsby-v1/src/pages/apply.js
@@ -0,0 +1,149 @@
+import React from 'react';
+
+const Apply = () => {
+  return (
+    <section id="contact" className="pad-regular section bg-default">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="contactus-brief">
+              <h3>Apply</h3>
+              <p className="section-description">
+                Thank thank you for choosing to apply to Vets Who Code. We work
+                really hard to train our veterans and to maintain an inclusive
+                enviroment so our troops can truly thrive. Please fill out the
+                form below and we will contact you soon.
+              </p>
+              <form id="s2do-form" action="#" method="POST">
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputName" className="dark-text">
+                      Name
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Name"
+                      name="name"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputEmail" className="dark-text">
+                      Email
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Email"
+                      name="email"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputService" className="dark-text">
+                      Military Branch Of Service
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Thank you for your service"
+                      name="branch-of-service"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="Experience" className="dark-text">
+                      Experience
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Do you program and if so for how long?"
+                      name="experience"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputPortfolio" className="dark-text">
+                      Github, Portfolio or Linkedin
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Share your work"
+                      name="github-portfolio-or-linkedin"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputLocation" className="dark-text">
+                      Location ( City and State )
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Location"
+                      name="location"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputMRE" className="dark-text">
+                      Favorite MRE?
+                      <super>*</super>
+                    </label>
+                    <input
+                      className="form-control input-lg"
+                      type="text"
+                      placeholder="Chilli Mac?"
+                      name="favorite-mre"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <label for="InputInterviewStory" className="dark-text">
+                      Tell Us About Yourself
+                    </label>
+                    <textarea
+                      className="form-control"
+                      rows="7"
+                      placeholder="Here we focus on aptitude and impact, so tell us about yourself and why you want to join #VetsWhoCode?"
+                      name="tell-us-about-yourself"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <input
+                      type="submit"
+                      name="submit"
+                      href="#"
+                      className="btn btn-charity-default"
+                      title=""
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Apply;
