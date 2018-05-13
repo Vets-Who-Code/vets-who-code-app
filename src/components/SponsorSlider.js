66ca5f849ce5c5320df946f03a2ec5f550fdc19a
Andrew James <ajames20@gmail.com>
2018-05-13 11:44:41 -0600

    Add links to sponsors

--- src/components/SponsorSlider.js
+++ src/components/SponsorSlider.js
@@ -41,37 +41,37 @@ const settings = {
 const SponsorSlider = () => (
   <Slider {...settings}>
     <div>
-      <a href="#">
+      <a href="https://www.pluralsight.com/" target="_blank" rel="noopener noreferrer">
         <img src={pluralsightPNG} alt="Pluralsight" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://basecamp.com/" target="_blank" rel="noopener noreferrer">
         <img src={basecampPNG} alt="Basecamp" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://repl.it" target="_blank" rel="noopener noreferrer">
         <img src={replItPNG} alt="repl.it" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://slack.com/" target="_blank" rel="noopener noreferrer">
         <img src={slackPNG} alt="Slack" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://www.digitalocean.com/" target="_blank" rel="noopener noreferrer">
         <img src={doPNG} alt="Digital Ocean" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://www.interviewcake.com/" target="_blank" rel="noopener noreferrer">
         <img src={icPNG} alt="Interview Cake" height="60" width="60" />
       </a>
     </div>
     <div>
-      <a href="#">
+      <a href="https://www.honeybadger.io/" target="_blank" rel="noopener noreferrer">
         <img src={honeyBadgerPNG} alt="Honeybadger" height="60" width="60" />
       </a>
     </div>
