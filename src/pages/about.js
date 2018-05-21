a9ee19f554b5321e320bf61529dfb5245757429c
Andrew James <ajames20@gmail.com>
2018-05-20 20:44:45 -0600

    Add basic outline for video on About me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-import vwcGIF from '../images/vwc.gif'
-import jerome from '../images/team/jerome.png'
-import noel from '../images/team/noel.png'
-import andrew from '../images/team/andrew.png'
-import thisIsUs from '../images/this_is_us.png'
+import vwcGIF from '../images/vwc.gif';
+import jerome from '../images/team/jerome.png';
+import noel from '../images/team/noel.png';
+import andrew from '../images/team/andrew.png';
+import thisIsUs from '../images/this_is_us.png';
+
+import facebookVideo from '../video/vwc-facebook-reel.mp4';
 
 const About = () => (
   <div>
@@ -35,38 +37,31 @@ const About = () => (
             <h3 className="story-title">Our Success story</h3>
           </div>
           <div className="col-md-5">
-            <div
-              className="success-story-video"
-              style={{ backgroundImage: `url(${vwcGIF})` }}
-            >
+            <div className="success-story-video" style={{ backgroundImage: `url(${vwcGIF})` }}>
+              <video className="img-responsive" style={{ width: 457, height: 320 }}>
+                <source src={facebookVideo} />
+              </video>
               <a className="popup-vimeo" href="//vimeo.com/45830194">
                 <i className="fa fa-play-circle-o" />
-              </a>{' '}
+              </a>
             </div>
           </div>
           <div className="col-md-7">
             <div className="success-story">
               <p className="story">
-                Launched in 2014, Vets Who Code is a non-profit dedicated to
-                filling the nations technical skills gap with America’s best. We
-                achieve this by using technology to connect and train veterans
-                remotely in web development in order to close the digital talent
-                gap and ease career transition for military veterans and to give
-                military spouses skills to provide stability as they move to
-                support their families. We believe that those who serve in
-                uniform can be the digital economy’s most productive and
-                innovative . Vets Who Code prepares them to enter the civilian
-                work force with tangible skills for new careers.
+                Launched in 2014, Vets Who Code is a non-profit dedicated to filling the nations technical skills gap
+                with America’s best. We achieve this by using technology to connect and train veterans remotely in web
+                development in order to close the digital talent gap and ease career transition for military veterans
+                and to give military spouses skills to provide stability as they move to support their families. We
+                believe that those who serve in uniform can be the digital economy’s most productive and innovative .
+                Vets Who Code prepares them to enter the civilian work force with tangible skills for new careers.
               </p>
               <p className="story-last">
-                Vets Who Code is a reintegration solution for veterans that
-                believes in the principle of "To Teach a Man To Fish" to better
-                prepare early stage transitioning veterans for returning to the
-                workforce. Many veterans are not homeless, but are un- and
-                under-employed. Some have a deep desire to be independent
-                business owners. Their skill sets are desperately needed to
-                enhance local economies and to drive innovation. Vets Who Code
-                serves as a launch pad for our veterans and military spouses.
+                Vets Who Code is a reintegration solution for veterans that believes in the principle of "To Teach a Man
+                To Fish" to better prepare early stage transitioning veterans for returning to the workforce. Many
+                veterans are not homeless, but are un- and under-employed. Some have a deep desire to be independent
+                business owners. Their skill sets are desperately needed to enhance local economies and to drive
+                innovation. Vets Who Code serves as a launch pad for our veterans and military spouses.
               </p>
             </div>
           </div>
@@ -81,7 +76,7 @@ const About = () => (
             <div className="row">
               <div className="col-md-4 col-sm-6">
                 <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={jerome} alt="Image" />
+                  <img className="img-responsive" src={jerome} alt="Jerome Hardaway Head Geek" />
                   <div className="meet-social">
                     <a href="https://twitter.com/JeromeHardaway">
                       <i className="fa fa-twitter" />
@@ -101,7 +96,7 @@ const About = () => (
               </div>
               <div className="col-md-4 col-sm-6">
                 <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={andrew} alt="Image" />
+                  <img className="img-responsive" src={andrew} alt="Andrew Lebowitz Brand Geek" />
                   <div className="meet-social">
                     <a href="https://twitter.com/witzcreative">
                       <i className="fa fa-twitter" />
@@ -122,7 +117,7 @@ const About = () => (
 
               <div className="col-md-4 col-sm-6">
                 <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={noel} alt="Image" />
+                  <img className="img-responsive" src={noel} alt="Noel Sagaille Devops Geek" />
                   <div className="meet-social">
                     <a href="https://twitter.com/MrBernnz">
                       <i className="fa fa-twitter" />
@@ -146,6 +141,6 @@ const About = () => (
       </div>
     </section>
   </div>
-)
+);
 
-export default About
+export default About;
