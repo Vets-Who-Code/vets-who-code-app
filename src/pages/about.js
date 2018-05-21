686b736e62391ae622c87ef6b624f40168f5367d
Andrew James <ajames20@gmail.com>
2018-05-21 12:48:53 -0600

    Add intial markup to conditionaly render video

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
 import vwcGIF from '../images/vwc.gif';
@@ -9,138 +9,181 @@ import thisIsUs from '../images/this_is_us.png';
 
 import facebookVideo from '../video/vwc-facebook-reel.mp4';
 
-const About = () => (
-  <div>
-    <header
-      className="inner-header overlay grey text-center slim-bg "
-      style={{
-        backgroundImage: `url(${thisIsUs})`,
-        backgroundPositionY: 'bottom',
-      }}
-    >
-      <div className="overlay-01" />
-      <div className="container">
-        <h2 className="text-center text-uppercase">About</h2>
-        <div className="breadcrumb">
-          <Link to="/">Home</Link>
-          <span>/</span>
-          <Link to="/about" className="page-active">
-            About
-          </Link>
-        </div>
-      </div>
-    </header>
-    <section id="about" className="small-top-pad section bg-default">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h3 className="story-title">Our Success story</h3>
-          </div>
-          <div className="col-md-5">
-            <div className="success-story-video" style={{ backgroundImage: `url(${vwcGIF})` }}>
-              <video className="img-responsive" style={{ width: 457, height: 320 }}>
-                <source src={facebookVideo} />
-              </video>
-              <a className="popup-vimeo" href="//vimeo.com/45830194">
-                <i className="fa fa-play-circle-o" />
-              </a>
-            </div>
-          </div>
-          <div className="col-md-7">
-            <div className="success-story">
-              <p className="story">
-                Launched in 2014, Vets Who Code is a non-profit dedicated to filling the nations technical skills gap
-                with America’s best. We achieve this by using technology to connect and train veterans remotely in web
-                development in order to close the digital talent gap and ease career transition for military veterans
-                and to give military spouses skills to provide stability as they move to support their families. We
-                believe that those who serve in uniform can be the digital economy’s most productive and innovative .
-                Vets Who Code prepares them to enter the civilian work force with tangible skills for new careers.
-              </p>
-              <p className="story-last">
-                Vets Who Code is a reintegration solution for veterans that believes in the principle of "To Teach a Man
-                To Fish" to better prepare early stage transitioning veterans for returning to the workforce. Many
-                veterans are not homeless, but are un- and under-employed. Some have a deep desire to be independent
-                business owners. Their skill sets are desperately needed to enhance local economies and to drive
-                innovation. Vets Who Code serves as a launch pad for our veterans and military spouses.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section id="team" className="section bg-default">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h3 className="meet-us-heading">Our Team</h3>
-            <div className="row">
-              <div className="col-md-4 col-sm-6">
-                <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={jerome} alt="Jerome Hardaway Head Geek" />
-                  <div className="meet-social">
-                    <a href="https://twitter.com/JeromeHardaway">
-                      <i className="fa fa-twitter" />
-                    </a>
-                    <a href="https://github.com/jeromehardaway">
-                      <i className="fa fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/jeromehardaway/">
-                      <i className="fa fa-linkedin" />
-                    </a>
-                  </div>
-                  <div className="personal-info">
-                    <p className="title">Jerome Hardaway</p>
-                    <p className="designation">Head Geek</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-sm-6">
-                <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={andrew} alt="Andrew Lebowitz Brand Geek" />
-                  <div className="meet-social">
-                    <a href="https://twitter.com/witzcreative">
-                      <i className="fa fa-twitter" />
-                    </a>
-                    <a href="https://www.behance.net/WitzCreative">
-                      <i className="fa fa-behance" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/witzcreative">
-                      <i className="fa fa-linkedin" />
-                    </a>
-                  </div>
-                  <div className="personal-info">
-                    <p className="title">Andrew Lebowitz</p>
-                    <p className="designation">Brand Geek</p>
-                  </div>
-                </div>
-              </div>
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.play = this.play.bind(this);
+  }
 
-              <div className="col-md-4 col-sm-6">
-                <div className="team-wiget clearfix">
-                  <img className="img-responsive" src={noel} alt="Noel Sagaille Devops Geek" />
-                  <div className="meet-social">
-                    <a href="https://twitter.com/MrBernnz">
-                      <i className="fa fa-twitter" />
-                    </a>
-                    <a href="https://github.com/mrbernnz">
-                      <i className="fa fa-github" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/mrbernnz">
-                      <i className="fa fa-linkedin" />
+  play() {
+    $('.play-button')
+      .delay(100)
+      .fadeOut();
+    $('.success-story-video')
+      .delay(100)
+      .fadeOut()
+      .css('background-image', 'none');
+    $('.success-video')
+      .fadeIn()
+      .css('display', 'block');
+    this.refs.video.play();
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <header
+            className="inner-header overlay grey text-center slim-bg "
+            style={{
+              backgroundImage: `url(${thisIsUs})`,
+              backgroundPositionY: 'bottom',
+            }}
+          >
+            <div className="overlay-01" />
+            <div className="container">
+              <h2 className="text-center text-uppercase">About</h2>
+              <div className="breadcrumb">
+                <Link to="/">Home</Link>
+                <span>/</span>
+                <Link to="/about" className="page-active">
+                  About
+                </Link>
+              </div>
+            </div>
+          </header>
+          <section id="about" className="small-top-pad section bg-default">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h3 className="story-title">Our Success story</h3>
+                </div>
+                <div className="col-md-5">
+                  <div
+                    className="success-story-video"
+                    style={{ backgroundImage: `url(${vwcGIF})` }}
+                    onClick={this.play}
+                  >
+                    <a className="play-button">
+                      <i className="fa fa-play-circle-o" style={{ color: 'grey' }} />
                     </a>
                   </div>
-                  <div className="personal-info">
-                    <p className="title">Noel Sagaille</p>
-                    <p className="designation">Devops Geek</p>
+                  <video
+                    className="success-video"
+                    poster={{ backgroundImage: `url(${vwcGIF})` }}
+                    ref="video"
+                    className="img-responsive"
+                    muted
+                    style={{ display: 'hidden' }}
+                  >
+                    <source src={facebookVideo} />
+                  </video>
+                </div>
+                <div className="col-md-7">
+                  <div className="success-story">
+                    <p className="story">
+                      Launched in 2014, Vets Who Code is a non-profit dedicated to filling the nations technical skills
+                      gap with America’s best. We achieve this by using technology to connect and train veterans
+                      remotely in web development in order to close the digital talent gap and ease career transition
+                      for military veterans and to give military spouses skills to provide stability as they move to
+                      support their families. We believe that those who serve in uniform can be the digital economy’s
+                      most productive and innovative . Vets Who Code prepares them to enter the civilian work force with
+                      tangible skills for new careers.
+                    </p>
+                    <p className="story-last">
+                      Vets Who Code is a reintegration solution for veterans that believes in the principle of "To Teach
+                      a Man To Fish" to better prepare early stage transitioning veterans for returning to the
+                      workforce. Many veterans are not homeless, but are un- and under-employed. Some have a deep desire
+                      to be independent business owners. Their skill sets are desperately needed to enhance local
+                      economies and to drive innovation. Vets Who Code serves as a launch pad for our veterans and
+                      military spouses.
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </section>
+          <section id="team" className="section bg-default">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <h3 className="meet-us-heading">Our Team</h3>
+                  <div className="row">
+                    <div className="col-md-4 col-sm-6">
+                      <div className="team-wiget clearfix">
+                        <img className="img-responsive" src={jerome} alt="Jerome Hardaway Head Geek" />
+                        <div className="meet-social">
+                          <a href="https://twitter.com/JeromeHardaway">
+                            <i className="fa fa-twitter" />
+                          </a>
+                          <a href="https://github.com/jeromehardaway">
+                            <i className="fa fa-github" />
+                          </a>
+                          <a href="https://www.linkedin.com/in/jeromehardaway/">
+                            <i className="fa fa-linkedin" />
+                          </a>
+                        </div>
+                        <div className="personal-info">
+                          <p className="title">Jerome Hardaway</p>
+                          <p className="designation">Head Geek</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                      <div className="team-wiget clearfix">
+                        <img className="img-responsive" src={andrew} alt="Andrew Lebowitz Brand Geek" />
+                        <div className="meet-social">
+                          <a href="https://twitter.com/witzcreative">
+                            <i className="fa fa-twitter" />
+                          </a>
+                          <a href="https://www.behance.net/WitzCreative">
+                            <i className="fa fa-behance" />
+                          </a>
+                          <a href="https://www.linkedin.com/in/witzcreative">
+                            <i className="fa fa-linkedin" />
+                          </a>
+                        </div>
+                        <div className="personal-info">
+                          <p className="title">Andrew Lebowitz</p>
+                          <p className="designation">Brand Geek</p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6">
+                      <div className="team-wiget clearfix">
+                        <img className="img-responsive" src={noel} alt="Noel Sagaille Devops Geek" />
+                        <div className="meet-social">
+                          <a href="https://twitter.com/MrBernnz">
+                            <i className="fa fa-twitter" />
+                          </a>
+                          <a href="https://github.com/mrbernnz">
+                            <i className="fa fa-github" />
+                          </a>
+                          <a href="https://www.linkedin.com/in/mrbernnz">
+                            <i className="fa fa-linkedin" />
+                          </a>
+                        </div>
+                        <div className="personal-info">
+                          <p className="title">Noel Sagaille</p>
+                          <p className="designation">Devops Geek</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
-    </section>
-  </div>
-);
+    );
+  }
+}
 
 export default About;
+// const About = () => (
+
+// );
+
+// export default About;
