019f974890814bb6299c938c340abc4f02f9f347
Andrew James <ajames20@gmail.com>
2018-08-05 22:06:02 -0600

    Failed test for class name being added to class, test is skipped

--- tests/components/Nav.test.js
+++ tests/components/Nav.test.js
@@ -6,6 +6,18 @@ describe('<Nav />', () => {
 
   beforeEach(() => wrapper = shallow(<Nav />))
 
+  test.skip('should ', () => {
+    global.$ = jest.fn().mockImplementation(() =>({
+      scrollTop: jest.fn(),
+      removeClass: jest.fn(),
+      blur: jest.fn()
+    }))
+    global.scrollTo = 50
+    global.dispatchEvent(new Event('scroll'))
+    console.log(wrapper.debug())
+    expect(wrapper.find('nav').hasClass('navbar-solid')).toBe(true)
+  })
+
   test('should render correctly', () => {
     expect(wrapper).toMatchSnapshot()
   })
