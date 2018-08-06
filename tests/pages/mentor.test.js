ef5e897538ade0c9692a85e58983140e377d3f8e
Andrew James <ajames20@gmail.com>
2018-08-06 08:24:04 -0600

    Add test coverage for mentor.js in pages

--- tests/pages/mentor.test.js
+++ tests/pages/mentor.test.js
@@ -18,6 +18,47 @@ describe('<Mentor />', () => {
     expect(wrapper.state('name')).toEqual('User Name')
   })
 
+  test('should call fetch with correct options when handleSubmit is invoked', () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve())
+    const mockEvent = {
+      preventDefault: jest.fn()
+    }
+    wrapper.setState({
+      name: 'User Name',
+      email: 'me@mail.com',
+      'branch-of-service': 'branch',
+      'technical-expertise': 'typing',
+      'github-portfolio-or-linkedin': 'github-url',
+      location: 'some state usa',
+      'employer-restrictions': 'n/a'
+    })
+    const mockUrl = 'https://eec3hqm275.execute-api.us-east-1.amazonaws.com/prod/mentor'
+    const mockOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'User Name',
+        email: 'me@mail.com',
+        'branch-of-service': 'branch',
+        'technical-expertise': 'typing',
+        'github-portfolio-or-linkedin': 'github-url',
+        location: 'some state usa',
+        'employer-restrictions': 'n/a'
+      })
+    }
+    wrapper.instance().handleSubmit(mockEvent)
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl, mockOptions)
+  })
+
+  test('should invoke resetForm when handleSubmit is invoked', () => {
+    const spy = jest.spyOn(wrapper.instance(), 'resetForm')
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve())
+    const mockEvent = {
+      preventDefault: jest.fn()
+    }
+    wrapper.instance().handleSubmit(mockEvent)
+    expect(spy).toHaveBeenCalled()
+  })
+
   test('should render correctly', () => {
     expect(wrapper).toMatchSnapshot()
   })
