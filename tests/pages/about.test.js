2fecbe147245e38134a2a651353f1b0fd3d30972
Andrew James <ajames20@gmail.com>
2018-08-06 07:54:35 -0600

    Add test coverage for about.js in pages

--- tests/pages/about.test.js
+++ tests/pages/about.test.js
@@ -6,13 +6,25 @@ describe('<About />', () => {
   let wrapper
 
   beforeEach(() => {
-    wrapper = mount(<About />)
+    wrapper = shallow(<About />)
     global.$ = global.jQuery = $
   })
 
-  test.skip('should invoke play method on click', () => {
+  test('should invoke play method on click', () => {
+    wrapper = mount(<About />)
+
+    global.HTMLMediaElement.prototype.play = () => ({})
     const spy = jest.spyOn(wrapper.instance(), 'play')
+    wrapper.instance().forceUpdate()
     wrapper.find('.play-button').simulate('click')
+    expect(spy).toHaveBeenCalled()
+  })
+
+  test('should invoke end method when video completes', () => {
+    wrapper = mount(<About />)
+    const spy = jest.spyOn(wrapper.instance(), 'end')
+    window.HTMLMediaElement.prototype.end = wrapper.instance().end()
+    wrapper.instance().forceUpdate()
     expect(spy).toHaveBeenCalled()
   })
 
