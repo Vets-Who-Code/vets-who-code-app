d23415868de6a08e8ba2c5549bb9841893d6c44e
Andrew James <ajames20@gmail.com>
2018-09-26 08:17:14 -0600

    Add test coverage for index.js

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import IndexPaege from '../../src/pages/index'
+import React from 'react';
+import IndexPaege from '../../src/pages/index';
 
 describe('<IndexPaege />', () => {
-  let wrapper
+  let wrapper;
 
-  beforeEach(() => wrapper = shallow(<IndexPaege />))
+
+  beforeEach(() => wrapper = shallow(<IndexPaege />));
+
+  test('should update email property in state onChange', () => {
+    const expected = 'someuser@mail.com';
+    const mockEvent = {
+      target: {
+        name: 'email',
+        value: expected
+      }
+    };
+    wrapper.instance().handleInputChange(mockEvent);
+    expect(wrapper.state('email')).toEqual(expected);
+  });
+
+  test('should call window.fetch when handleUserSubscribe is invoked', () => {
+    const mockEvent = {
+      preventDefault: jest.fn()
+    };
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve());
+
+    wrapper.instance().handleUserSubscribe(mockEvent);
+    expect(window.fetch).toHaveBeenCalled();
+  });
+
 
   test('should render correctly', () => {
-    expect(wrapper).toMatchSnapshot()
-  })
-})
+    expect(wrapper).toMatchSnapshot();
+  });
+});
