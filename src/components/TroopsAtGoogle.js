c483637e18edaae5239e0cbda115c00df688a74c
Andrew James <ajames20@gmail.com>
2018-10-05 19:48:21 -0500

    Add TroopsAtGoogle component to usr remark images

---
+++ src/components/TroopsAtGoogle.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Img from 'gatsby-image'
+
+export const TroopsAtGoogleImage = ({ data }) => <Img fluid={data.file.childImageSharp.fluid} />
+
+export const TroopsAtGoogle = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        file(relativePath: { eq: "troops-at-google.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => <TroopsAtGoogleImage data={data} {...props} />}
+  />
+)
+
+TroopsAtGoogleImage.propTypes = {
+  data: PropTypes.object,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 import Countdown from '../components/Countdown'
 import Header from '../components/Header'
 
-import troopsAtGooglePNG from '../images/troops-at-google.jpg'
+import { TroopsAtGoogle } from '../components/TroopsAtGoogle'
 
 class IndexPage extends Component {
   state = {
@@ -44,136 +44,130 @@ class IndexPage extends Component {
     return (
       <Layout>
         <Header />
-        <div className="fade-in">
-          <section
-            id="our_stories"
-            className="section pad-regular bg-default our_stories small-top-pad"
-          >
-            <div className="container">
-              <div className="row bg-dark">
-                <div className="col-md-5 col-sm-12 no_left_pad no_right_pad">
-                  <img
-                    className="img-responsive"
-                    src={troopsAtGooglePNG}
-                    alt="Vets Who Code veterans at Google event."
-                  />
+        <section
+          id="our_stories"
+          className="section pad-regular bg-default our_stories small-top-pad"
+        >
+          <div className="container">
+            <div className="row bg-dark">
+              <div className="col-md-5 col-sm-12 no_left_pad no_right_pad">
+                <TroopsAtGoogle className="img-responsive" />
+              </div>
+              <div className="col-md-7 col-sm-12 our_story_content text-center">
+                <div className="featured-heading">
+                  <h2>#VetsWhoCode</h2>
                 </div>
-                <div className="col-md-7 col-sm-12 our_story_content text-center">
-                  <div className="featured-heading">
-                    <h2>#VetsWhoCode</h2>
-                  </div>
+                <p>
+                  #VetsWhoCode is a veteran-led and operated 501(c)(3) charitable non-profit that
+                  focuses on training veterans in web development and software engineering
+                  principles free of charge with the focus of starting careers as javascript
+                  developers.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="our_missions" className="section pad-regular bg-light our_missions">
+          <div className="container">
+            <div className="row">
+              <div className="featured-heading text-center">
+                <h2 className="dark_color">
+                  RETOOL
+                  <i className="fa fa-code" aria-hidden="true" /> RETRAIN
+                  <i className="fa fa-code" aria-hidden="true" /> RELAUNCH
+                </h2>
+              </div>
+              <div className="col-sm-12 our_mission_content text-center">
+                <blockquote>
                   <p>
-                    #VetsWhoCode is a veteran-led and operated 501(c)(3) charitable non-profit that
-                    focuses on training veterans in web development and software engineering
-                    principles free of charge with the focus of starting careers as javascript
-                    developers.
+                    Through training, building projects, mentorship, and opportunities to connect
+                    with industry experts, we ensure that our troops gain the best knowledge
+                    possible in web development and software engineering.
                   </p>
-                </div>
+                </blockquote>
               </div>
             </div>
-          </section>
-          <section id="our_missions" className="section pad-regular bg-light our_missions">
-            <div className="container">
-              <div className="row">
-                <div className="featured-heading text-center">
-                  <h2 className="dark_color">
-                    RETOOL
-                    <i className="fa fa-code" aria-hidden="true" /> RETRAIN
-                    <i className="fa fa-code" aria-hidden="true" /> RELAUNCH
-                  </h2>
-                </div>
-                <div className="col-sm-12 our_mission_content text-center">
-                  <blockquote>
-                    <p>
-                      Through training, building projects, mentorship, and opportunities to connect
-                      with industry experts, we ensure that our troops gain the best knowledge
-                      possible in web development and software engineering.
-                    </p>
-                  </blockquote>
-                </div>
+          </div>
+        </section>
+        <section
+          id="causes"
+          className="section bg-default causes-card"
+          style={{ padding: '75px 0' }}
+        >
+          <div className="container">
+            <div className="normal_heading text-center">
+              <h2>Our Partners</h2>
+            </div>
+            <div className="row">
+              <div className="col-sm-12 cause_content text-center" style={{ marginBottom: 40 }}>
+                <h4 className="cause_title">Thank You For Working With #VetsWhoCode!</h4>
+                <hr />
+                <h3 className="subtitle">
+                  We Are Grateful To Have These Companies Partner With Us On Our Journey To Train
+                  Veterans How To Code!.
+                </h3>
               </div>
             </div>
-          </section>
-          <section
-            id="causes"
-            className="section bg-default causes-card"
-            style={{ padding: '75px 0' }}
-          >
-            <div className="container">
-              <div className="normal_heading text-center">
-                <h2>Our Partners</h2>
-              </div>
-              <div className="row">
-                <div className="col-sm-12 cause_content text-center" style={{ marginBottom: 40 }}>
-                  <h4 className="cause_title">Thank You For Working With #VetsWhoCode!</h4>
-                  <hr />
-                  <h3 className="subtitle">
-                    We Are Grateful To Have These Companies Partner With Us On Our Journey To Train
-                    Veterans How To Code!.
-                  </h3>
+            <SponsorSlider />
+          </div>
+        </section>
+        <section id="event_card" className="section bg-dark pad-regular event_card">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6 event_content">
+                <h4>Next Cohort</h4>
+                <div className="event_excerpt">
+                  <p>Countdown until our next #VetsWhoCode Cohort gets started:</p>
                 </div>
               </div>
-              <SponsorSlider />
+              <Countdown />
             </div>
-          </section>
-          <section id="event_card" className="section bg-dark pad-regular event_card">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-6 event_content">
-                  <h4>Next Cohort</h4>
-                  <div className="event_excerpt">
-                    <p>Countdown until our next #VetsWhoCode Cohort gets started:</p>
-                  </div>
-                </div>
-                <Countdown />
-              </div>
-            </div>
-          </section>
-          <section id="newsletter_card" className="section bg-light pad-regular newsletter_card">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-12 newsletter_wrapper">
-                  <div className="newsletter_inner_wrapper">
-                    <div className="row">
-                      <div className="col-md-4">
-                        <h3 className="text-center">JOIN OUR EMAIL LIST</h3>
-                      </div>
-                      <div className="col-md-8">
-                        <form id="s2do-form" onSubmit={this.handleUserSubscribe}>
-                          <div className="row">
-                            <div className="col-md-9">
-                              <div className="form-group">
-                                <input
-                                  name="email"
-                                  type="email"
-                                  value={email}
-                                  onChange={this.handleInputChange}
-                                  id="subscriber-email"
-                                  className="form-control"
-                                  placeholder="Enter your email address"
-                                  required
-                                />
-                              </div>
-                            </div>
-                            <div className="col-md-3 subscribe-button_box">
-                              <button
-                                id="subscribe-button"
-                                type="submit"
-                                className="btn btn-subscribe"
-                              >
-                                subscribe
-                              </button>
+          </div>
+        </section>
+        <section id="newsletter_card" className="section bg-light pad-regular newsletter_card">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 newsletter_wrapper">
+                <div className="newsletter_inner_wrapper">
+                  <div className="row">
+                    <div className="col-md-4">
+                      <h3 className="text-center">JOIN OUR EMAIL LIST</h3>
+                    </div>
+                    <div className="col-md-8">
+                      <form id="s2do-form" onSubmit={this.handleUserSubscribe}>
+                        <div className="row">
+                          <div className="col-md-9">
+                            <div className="form-group">
+                              <input
+                                name="email"
+                                type="email"
+                                value={email}
+                                onChange={this.handleInputChange}
+                                id="subscriber-email"
+                                className="form-control"
+                                placeholder="Enter your email address"
+                                required
+                              />
                             </div>
                           </div>
-                        </form>
-                      </div>
+                          <div className="col-md-3 subscribe-button_box">
+                            <button
+                              id="subscribe-button"
+                              type="submit"
+                              className="btn btn-subscribe"
+                            >
+                              subscribe
+                            </button>
+                          </div>
+                        </div>
+                      </form>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
       </Layout>
     )
   }
