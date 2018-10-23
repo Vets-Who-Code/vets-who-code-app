7831822a02a8fb8086fb9ea6ad5edd2a842c4a42
Andrew James <ajames20@gmail.com>
2018-10-23 05:21:20 -0500

    Move Footer component to react-testin-lib

--- tests/components/Footer.test.js
+++ tests/components/Footer.test.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Footer from '../../src/components/Footer'
+import { render } from 'react-testing-library'
 
 describe('<Footer />', () => {
   let wrapper
 
-  beforeEach(() => wrapper = shallow(<Footer />))
+  beforeEach(() => wrapper = render(<Footer />))
 
   test('should render correctly', () => {
+    const { container } = wrapper;
     expect(wrapper).toMatchSnapshot()
   })
 
