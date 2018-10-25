7ffd0a4f43e7c6d88ced2604defd025cb2b8b75c
Andrew James <ajames20@gmail.com>
2018-10-25 06:04:48 -0500

    Add async in bable and update Countdown test

--- tests/components/Countdown.test.js
+++ tests/components/Countdown.test.js
@@ -1,51 +1,17 @@
 import React from 'react'
 import Countdown from '../../src/components/Countdown'
+import { render, waitForElement, getByText } from 'react-testing-library'
 
 jest.useFakeTimers()
 
 describe('<Countdown />', () => {
-  let wrapper
+  test('should unmount and clear interval to prevent memory leaks', () => {
+    const { unmount } = render(<Countdown />)
 
-  beforeEach(() => wrapper = shallow(
-    <Countdown />
-  ))
+    jest.spyOn(console, 'error').mockImplementation(() => {})
 
-  test('should set interval to a number that can be tracked in local state', () => {
-    wrapper.setState({
-      interval: null
-    })
-    wrapper.instance().componentDidMount()
-    const result = wrapper.state('interval')
-    expect(result).toBe(2)
-  })
-
-  test('should call stopCountDown when componentWillUnmount', () => {
-    const spy = jest.spyOn(wrapper.instance(), 'stopCountDown')
-    wrapper.instance().componentWillUnmount()
-    expect(spy).toHaveBeenCalled()
-  })
-
-  test('should update state when getTimeRemaining is invoked', () => {
-    wrapper.instance().getTimeRemaining()
-    expect(wrapper.state('days')).not.toBe(null)
-    expect(wrapper.state('hours')).not.toBe(null)
-    expect(wrapper.state('minutes')).not.toBe(null)
-    expect(wrapper.state('seconds')).not.toBe(null)
-  })
-
-
-  test('should clear interval when stopCountDown is invoked', () => {
-    wrapper.instance().stopCountDown()
-    expect(clearInterval).toHaveBeenCalledWith(expect.any(Number))
-  })
-
-  test('should render correctly', () => {
-    wrapper.setState({
-      days: 1,
-      hours: 1,
-      minutes: 27,
-      seconds: 30
-    })
-    expect(wrapper).toMatchSnapshot()
+    unmount()
+    // eslint-disable-next-line
+    expect(console.error).not.toHaveBeenCalled()
   })
 })
