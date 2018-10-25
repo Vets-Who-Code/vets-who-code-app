731f9d454758fa140787ef8997f86c32193cc386
Andrew James <ajames20@gmail.com>
2018-10-25 17:54:22 -0500

    Update about page to react testing library
    Update about test file

--- tests/__mocks__/gatsby.js
+++ tests/__mocks__/gatsby.js
@@ -1,2 +1,2 @@
-const gatsby = jest.requireActual("gatsby");
-module.exports = { ...gatsby, graphql: jest.fn(), Link: "Link" };
+const gatsby = jest.requireActual("gatsby")
+module.exports = { ...gatsby, graphql: jest.fn(), Link: "Link" }
