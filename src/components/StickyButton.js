321f2b2c577f7752fd5d777d63389a9f7ce3a40f
Andrew James <ajames20@gmail.com>
2018-10-30 03:00:55 -0500

    Update after prettier

--- src/components/StickyButton.js
+++ src/components/StickyButton.js
@@ -6,14 +6,15 @@ const styles = {
   bottom: 28,
   zIndex: 20000,
   left: '12px',
-  padding: '1rem 1.5rem'
+  padding: '1rem 1.5rem',
 }
 
 const StickyButton = () => (
   <Link to="/donate">
-    <button className="btn btn-sm btn-charity-default" style={styles}>Donate</button>
+    <button className="btn btn-sm btn-charity-default" style={styles}>
+      Donate
+    </button>
   </Link>
 )
 
 export default StickyButton
-
