4ec4bbe547362afe0020b962616090219b6174da
Andrew James <ajames20@gmail.com>
2018-10-31 06:32:06 -0500

    Add sticky button to absolute left

--- src/components/StickyButton.js
+++ src/components/StickyButton.js
@@ -5,13 +5,13 @@ const styles = {
   position: 'fixed',
   bottom: 28,
   zIndex: 20000,
-  left: '12px',
+  left: 0,
   padding: '1rem 1.5rem',
 }
 
 const StickyButton = () => (
   <Link to="/donate">
-    <button className="btn btn-sm btn-charity-default" style={styles}>
+    <button className="btn btn-charity-default" style={styles}>
       Donate
     </button>
   </Link>
