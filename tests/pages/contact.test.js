fed9931d109d6db55e188276e299c533c6704fc8
Andrew James <ajames20@gmail.com>
2018-11-04 05:42:10 -0600

    Remove commented out tests from contact

--- tests/pages/contact.test.js
+++ tests/pages/contact.test.js
@@ -3,57 +3,11 @@ import Contact from '../../src/pages/contact'
 import { render, fireEvent } from 'react-testing-library'
 
 describe('<Contact />', () => {
-  // test('should update state handleChange is invoked', () => {
-  //   const mockEvent = {
-  //     target: {
-  //       name: 'name',
-  //       value: 'User Name'
-  //     }
-  //   }
-  //   wrapper.instance().handleChange(mockEvent)
-  //   expect(wrapper.state('name')).toEqual('User Name')
-  // })
-
-  // test('should call fetch with correct options when handleSubmit is invoked', () => {
-  //   window.fetch = jest.fn().mockImplementation(() => Promise.resolve())
-  //   const mockEvent = {
-  //     preventDefault: jest.fn()
-  //   }
-  //   wrapper.setState({
-  //     name: 'user name',
-  //     email: 'me@mail.com',
-  //     phone: '555-555-555',
-  //     message: 'I want to join'
-  //   })
-  //   const mockUrl = 'https://eec3hqm275.execute-api.us-east-1.amazonaws.com/prod/contact'
-  //   const mockOptions = {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       name: 'user name',
-  //       email: 'me@mail.com',
-  //       phone: '555-555-555',
-  //       message: 'I want to join'
-  //     })
-  //   }
-  //   wrapper.instance().handleSubmit(mockEvent)
-  //   expect(window.fetch).toHaveBeenCalledWith(mockUrl, mockOptions)
-  // })
-
-  // test('should invoke resetForm when handleSubmit is invoked', () => {
-  //   const spy = jest.spyOn(wrapper.instance(), 'resetForm')
-  //   window.fetch = jest.fn().mockImplementation(() => Promise.resolve())
-  //   const mockEvent = {
-  //     preventDefault: jest.fn()
-  //   }
-  //   wrapper.instance().handleSubmit(mockEvent)
-  //   expect(spy).toHaveBeenCalled()
-  // })
-
   test('should submit subscription form', async () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
       status: 200,
     }))
-    const { container, debug } = render(<Contact />)
+    const { container } = render(<Contact />)
     const applicationForm = container.querySelector('#s2do-form')
     const nameInput = container.querySelector('#InputName')
     const emailInput = container.querySelector('#InputEmail')
