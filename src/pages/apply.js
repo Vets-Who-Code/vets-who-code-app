c90b73f90065c419e8f52dff3889d319df29c216
Andrew James <ajames20@gmail.com>
2019-01-09 16:03:34 -0600

    Add conditional render based on state

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -14,8 +14,8 @@ export default class Apply extends Component {
     location: '',
     'favorite-mre': '',
     'tell-us-about-yourself': '',
-    message: 
-    'Thank you for choosing to apply to Vets Who Code. We work really hard to train our veterans and to maintain an inclusive enviroment so our troops can truly thrive. Please fill out the form below and we will contact you soon.',
+    message:
+      'Thank you for choosing to apply to Vets Who Code. We work really hard to train our veterans and to maintain an inclusive enviroment so our troops can truly thrive. Please fill out the form below and we will contact you soon.',
     formHeading: 'Apply',
     loading: false,
     formSuccess: false,
@@ -111,168 +111,172 @@ export default class Apply extends Component {
                 <div className="contactus-brief">
                   <h3>{formHeading}</h3>
                   <p className="section-description">{message}</p>
-                  <form 
-                    id="s2do-form" 
-                    action="#" 
-                    onSubmit={this.handleSubmit}
-                    style={{ display: formSuccess || formError ? 'none' : 'block' }}
-                  >
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="name" className="dark-text">
-                          Name
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="name"
-                          className="form-control input-lg"
-                          type="text"
-                          id="name"
-                          placeholder="Name"
-                          name="name"
-                          value={this.state.name}
-                          onChange={this.handleChange}
-                          required
-                        />
+                  {formError && <p>Some Form Error message Here</p>}
+                  {!formSuccess && ( // this is a common patter in
+                    //React that you will see all over the place
+                    <form
+                      id="s2do-form"
+                      action="#"
+                      onSubmit={this.handleSubmit}
+                      // this was a nice solution for real. I like what you did here.
+                      // style={{ display: formSuccess || formError ? 'none' : 'block' }}
+                    >
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="name" className="dark-text">
+                            Name
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="name"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Name"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputEmail" className="dark-text">
-                          Email
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="InputEmail"
-                          className="form-control input-lg"
-                          type="email"
-                          placeholder="Email"
-                          name="email"
-                          value={this.state.email}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputEmail" className="dark-text">
+                            Email
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="InputEmail"
+                            className="form-control input-lg"
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputService" className="dark-text">
-                          Military Branch Of Service
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="InputService"
-                          className="form-control input-lg"
-                          type="text"
-                          placeholder="Thank you for your service"
-                          name="branch-of-service"
-                          value={this.state['branch-of-service']}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputService" className="dark-text">
+                            Military Branch Of Service
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="InputService"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Thank you for your service"
+                            name="branch-of-service"
+                            value={this.state['branch-of-service']}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="Experience" className="dark-text">
-                          Experience
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="Experience"
-                          className="form-control input-lg"
-                          type="text"
-                          placeholder="Do you program and if so for how long?"
-                          name="experience"
-                          value={this.state.experience}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="Experience" className="dark-text">
+                            Experience
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="Experience"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Do you program and if so for how long?"
+                            name="experience"
+                            value={this.state.experience}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputPortfolio" className="dark-text">
-                          Github, Portfolio or Linkedin
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="InputPortfolio"
-                          className="form-control input-lg"
-                          type="text"
-                          placeholder="Share your work"
-                          name="github-portfolio-or-linkedin"
-                          value={this.state['github-portfolio-or-linkedin']}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputPortfolio" className="dark-text">
+                            Github, Portfolio or Linkedin
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="InputPortfolio"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Share your work"
+                            name="github-portfolio-or-linkedin"
+                            value={this.state['github-portfolio-or-linkedin']}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputLocation" className="dark-text">
-                          Location ( City and State )<sup>*</sup>
-                        </label>
-                        <input
-                          id="InputLocation"
-                          className="form-control input-lg"
-                          type="text"
-                          placeholder="Location"
-                          name="location"
-                          value={this.state.location}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputLocation" className="dark-text">
+                            Location ( City and State )<sup>*</sup>
+                          </label>
+                          <input
+                            id="InputLocation"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Location"
+                            name="location"
+                            value={this.state.location}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputMRE" className="dark-text">
-                          Favorite MRE?
-                          <sup>*</sup>
-                        </label>
-                        <input
-                          id="InputMRE"
-                          className="form-control input-lg"
-                          type="text"
-                          placeholder="Chilli Mac?"
-                          name="favorite-mre"
-                          value={this.state['favorite-mre']}
-                          onChange={this.handleChange}
-                          required
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputMRE" className="dark-text">
+                            Favorite MRE?
+                            <sup>*</sup>
+                          </label>
+                          <input
+                            id="InputMRE"
+                            className="form-control input-lg"
+                            type="text"
+                            placeholder="Chilli Mac?"
+                            name="favorite-mre"
+                            value={this.state['favorite-mre']}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <label htmlFor="InputInterviewStory" className="dark-text">
-                          Tell Us About Yourself
-                        </label>
-                        <textarea
-                          id="InputInterviewStory"
-                          className="form-control"
-                          rows="7"
-                          placeholder="Here we focus on aptitude and impact, so tell us about yourself
-                          and why you want to join #VetsWhoCode?"
-                          name="tell-us-about-yourself"
-                          value={this.state['tell-us-about-yourself']}
-                          onChange={this.handleChange}
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <label htmlFor="InputInterviewStory" className="dark-text">
+                            Tell Us About Yourself
+                          </label>
+                          <textarea
+                            id="InputInterviewStory"
+                            className="form-control"
+                            rows="7"
+                            placeholder="Here we focus on aptitude and impact, so tell us about yourself
+                              and why you want to join #VetsWhoCode?"
+                            name="tell-us-about-yourself"
+                            value={this.state['tell-us-about-yourself']}
+                            onChange={this.handleChange}
+                          />
+                        </div>
                       </div>
-                    </div>
-                    <div className="col-md-8">
-                      <div className="form-group">
-                        <input
-                          type="submit"
-                          name="submit"
-                          value={loading ? 'loading...' : 'submit'}
-                          href="#"
-                          className="btn btn-charity-default"
-                          title=""
-                        />
+                      <div className="col-md-8">
+                        <div className="form-group">
+                          <input
+                            type="submit"
+                            name="submit"
+                            value={loading ? 'loading...' : 'submit'}
+                            href="#"
+                            className="btn btn-charity-default"
+                            title=""
+                          />
+                        </div>
                       </div>
-                    </div>
-                  </form>
+                    </form>
+                  )}
                 </div>
               </div>
             </div>
