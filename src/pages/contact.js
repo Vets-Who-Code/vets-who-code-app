1d60dfbbab8cdab3f614d1e3d1e4a09ff6daf18c
ozzievee <o.vargas.bobg@gmail.com>
2019-01-14 08:44:54 -0500

    modifying contact.js making changes to contact.js in an effort to improve the form behavior. Added form state tracking, and associated behaviors based on the current form state. Also, modified contact.test.js.snap to account for page changes.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,6 +11,12 @@ export default class Contact extends Component {
     email: '',
     phone: '',
     message: '',
+    description:
+      'Fill out the from below and someone will contact you within 24 hours. Can&apos;t wait to hear from you!',
+    formHeading: 'Contact Us',
+    loading: false,
+    formSuccess: false,
+    formError: false,
   }
 
   componentDidMount() {
@@ -60,7 +66,9 @@ export default class Contact extends Component {
   })
 
   handleSubmit = e => {
-    const gatewayUrl = 'https://eec3hqm275.execute-api.us-east-1.amazonaws.com/prod/contact'
+    e.preventDefault()
+    //const gatewayUrl = 'https://eec3hqm275.execute-api.us-east-1.amazonaws.com/prod/contact'
+    const gatewayUrl = ''
     const options = {
       method: 'POST',
       body: JSON.stringify({
@@ -71,11 +79,27 @@ export default class Contact extends Component {
       }),
     }
     fetch(gatewayUrl, options)
-    e.preventDefault()
+      .then(resp => {
+        if (resp.ok) {
+          const description =
+            'Your application has been submitted successfully! ' +
+            'We look forward to contacting you soon.'
+          const formHeading = 'Thank You'
+          window.scrollTo(0, 0)
+          this.setState({ description, formSuccess: true, formHeading })
+        }
+      })
+      .catch(() => {
+        const formHeading = 'OOPS Some thing went wrong'
+        this.setState({ description, formError: true, formHeading })
+      })
+
     this.setState(this.resetForm)
   }
 
   render() {
+    const { formSuccess, description, formError, loading, formHeading } = this.state
+
     return (
       <Layout>
         <header
@@ -102,110 +126,118 @@ export default class Contact extends Component {
             <div className="row">
               <div className="col-xs-12">
                 <div className="contactus-brief">
-                  <h3>Contact us</h3>
-                  <p className="section-description">
-                    Fill out the from below and someone will contact you within 24 hours. Can&apos;t
-                    wait to hear from you!
+                  <h3>{formHeading}</h3>
+                  <p className={formSuccess ? 'alert alert-success' : 'section-description'}>
+                    {description}
                   </p>
+                  {formError && (
+                    <p className="alert alert-danger fade-in">
+                      There was an error trying to submit your application. Please try again.
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-12 clearfix">
-                <div id="map-canvas" className="map-default-height" />
+            {!formSuccess && (
+              <div className="row">
+                <div className="col-md-12 clearfix">
+                  <div id="map-canvas" className="map-default-height" />
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </section>
-        <section id="contact-form" className="section bg-default default-section-padding">
-          <div className="container">
-            <div className="row">
-              <div className="contact-form">
-                <form id="s2do-form" onSubmit={this.handleSubmit}>
-                  <div className="col-md-4">
-                    <div className="form-group">
-                      <label htmlFor="InputName" className="dark-text">
-                        Your Display Name
-                        <sup>*</sup>
-                      </label>
+        {!formSuccess && (
+          <section id="contact-form" className="section bg-default default-section-padding">
+            <div className="container">
+              <div className="row">
+                <div className="contact-form">
+                  <form id="s2do-form" onSubmit={this.handleSubmit}>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label htmlFor="InputName" className="dark-text">
+                          Your Display Name
+                          <sup>*</sup>
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control form-control-dark"
+                          name="name"
+                          id="InputName"
+                          placeholder="Jody"
+                          value={this.state.name}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label htmlFor="InputEmail" className="dark-text">
+                          Your Email Address
+                          <sup>*</sup>
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control form-control-dark"
+                          name="email"
+                          id="InputEmail"
+                          placeholder="jody@example.com"
+                          value={this.state.email}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="form-group">
+                        <label htmlFor="InputPhoneNumber" className="dark-text">
+                          Your Phone Number
+                        </label>
+                        <input
+                          type="number"
+                          className="form-control form-control-dark"
+                          name="phone"
+                          id="InputPhoneNumber"
+                          placeholder="123-456-789"
+                          value={this.state.phone}
+                          onChange={this.handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-sm-12">
+                      <div className="form-group">
+                        <label htmlFor="message" className="dark-text">
+                          Your Message
+                          <sup>*</sup>
+                        </label>
+                        <textarea
+                          id="message"
+                          className="form-control form-control-dark"
+                          rows="3"
+                          name="message"
+                          placeholder="Your Message Here.."
+                          value={this.state.message}
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </div>
                       <input
-                        type="text"
-                        className="form-control form-control-dark"
-                        name="name"
-                        id="InputName"
-                        placeholder="Jody"
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                        required
+                        id="cfsubmit"
+                        type="submit"
+                        name="submit"
+                        value="SEND MESSAGE"
+                        href="#"
+                        className="btn btn-charity-default"
+                        title=""
                       />
                     </div>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="form-group">
-                      <label htmlFor="InputEmail" className="dark-text">
-                        Your Email Address
-                        <sup>*</sup>
-                      </label>
-                      <input
-                        type="email"
-                        className="form-control form-control-dark"
-                        name="email"
-                        id="InputEmail"
-                        placeholder="jody@example.com"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                        required
-                      />
-                    </div>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="form-group">
-                      <label htmlFor="InputPhoneNumber" className="dark-text">
-                        Your Phone Number
-                      </label>
-                      <input
-                        type="number"
-                        className="form-control form-control-dark"
-                        name="phone"
-                        id="InputPhoneNumber"
-                        placeholder="123-456-789"
-                        value={this.state.phone}
-                        onChange={this.handleChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-sm-12">
-                    <div className="form-group">
-                      <label htmlFor="message" className="dark-text">
-                        Your Message
-                        <sup>*</sup>
-                      </label>
-                      <textarea
-                        id="message"
-                        className="form-control form-control-dark"
-                        rows="3"
-                        name="message"
-                        placeholder="Your Message Here.."
-                        value={this.state.message}
-                        onChange={this.handleChange}
-                        required
-                      />
-                    </div>
-                    <input
-                      id="cfsubmit"
-                      type="submit"
-                      name="submit"
-                      value="SEND MESSAGE"
-                      href="#"
-                      className="btn btn-charity-default"
-                      title=""
-                    />
-                  </div>
-                </form>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        )}
       </Layout>
     )
   }
