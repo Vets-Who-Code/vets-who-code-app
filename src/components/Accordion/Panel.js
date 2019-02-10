27c9099defd4c7694a828b6fbcbf273c4d3c2573
Andrew James <ajames20@gmail.com>
2019-02-10 07:12:20 -0600

    Add Spring styled-compnents fix bug

--- src/components/Accordion/Panel.js
+++ src/components/Accordion/Panel.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Spring } from 'react-spring/renderprops.cjs'
+import { Spring } from 'react-spring/renderprops.cjs' // please leave this import as is. The test sweet needs this to run properly
 
 const StyledPanel = styled.div`
+  padding: ${({ active }) => (active ? '40px 15px' : '0 15px')};
   height: ${({ active }) => (active ? 'auto' : 0)};
-  display: 'none';
-  opacity: ${({ active }) => (active ? 1 : 0)};
-  transition: all ease-in-out 0.2s;
+  will-change: height, padding;
+  transition: all ease 0.5s;
 `
 
 class Panel extends Component {
   static propTypes = {
     title: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
     body: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-    id: PropTypes.string,
+    id: PropTypes.number,
     accordionId: PropTypes.string,
+    activeId: PropTypes.number,
+    clickHandler: PropTypes.func,
   }
 
   state = {
     active: false,
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    const { activeId, single } = prevProps
+
+    if (single && this.props.activeId !== activeId && prevState.active) {
+      this.toggleActive()
+    }
   }
 
   handleDocumentClick = event => {
@@ -38,8 +48,13 @@ class Panel extends Component {
     document.removeEventListener('touchend', this.handleDocumentClick, false)
   }
 
+  toggleActive = () => {
+    this.setState(({ active }) => ({ active: !active }))
+  }
+
   handleClick = () => {
-    this.setState({ active: !this.state.active })
+    this.toggleActive()
+    this.props.clickHandler(this.props.id)
   }
 
   render() {
@@ -47,18 +62,16 @@ class Panel extends Component {
     const { active } = this.state
     return (
       <div
+        ref={ref => (this.panel = ref)}
         className={`panel panel-default ${active ? 'active' : ''}`}
-        onClick={this.handleClick}
-        ref={panel => (this.panel = panel)}
       >
-        <div className="panel-heading" role="tab" id={`heading${id}`}>
+        <div onClick={this.handleClick} className="panel-heading" role="tab">
           <h4 className="panel-title">
             <a
-              // className="collapsed"
+              className="collapsed"
               role="button"
               data-toggle="collapse"
               data-parent={`#${accordionId}`}
-              href={`#collapse${id}`}
               aria-expanded={active}
               aria-controls={`collapse${id}`}
             >
@@ -71,29 +84,25 @@ class Panel extends Component {
             </a>
           </h4>
         </div>
-        <Spring
-          from={{
-            height: 0,
-            opacity: 0,
-          }}
-          to={{
-            height: 'auto',
-            opacity: 1,
-          }}
+        <StyledPanel
+          role="tabpanel"
+          aria-labelledby={`heading${id}`}
+          aria-hidden={!active}
+          active={active}
         >
-          {springProps => (
-            <StyledPanel
-              id={`collapse${id}`}
-              style={{ springProps }}
-              role="tabpanel"
-              aria-labelledby={`heading${id}`}
-              aria-hidden={!active}
+          {active && (
+            <Spring
               active={active}
+              from={{ number: 0, opacity: 0 }}
+              to={{ number: 10, opacity: 1 }}
+              delay={50}
             >
-              <div className="panel-body">{typeof body === 'function' ? body() : body}</div>
-            </StyledPanel>
+              {springProps => (
+                <div style={springProps}>{typeof body === 'function' ? body() : body}</div>
+              )}
+            </Spring>
           )}
-        </Spring>
+        </StyledPanel>
       </div>
     )
   }
