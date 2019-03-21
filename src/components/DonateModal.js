d6577a5e3688083299ff2f39ff47007177d9dbe1
Andrew James <ajames20@gmail.com>
2019-03-21 06:48:47 -0500

    Add @reach/dialog for accesible modal

--- src/components/DonateModal.js
+++ src/components/DonateModal.js
@@ -1,48 +1,74 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Modal from 'react-modal'
+// import Transition from 'react-spring/renderprops.cjs'
+import { DialogOverlay, DialogContent } from '@reach/dialog'
 
-Modal.setAppElement('#___gatsby')
+import '@reach/dialog/styles.css'
 
-const container = {
-  content: {
-    top: '20%',
-    bottom: '20%',
-    height: 'auto',
-  },
+class DonateModal extends Component {
+  state = {
+    loading: true,
+  }
+
+  buttonRef = React.createRef()
+  iframeRef = React.createRef()
+
+  componentDidMount = () => {
+    // console.log(this.iframeRef)
+  }
+
+  handleIframeLoaded = () => {
+    this.setState({ loading: false })
+  }
+  render() {
+    return (
+      <>
+        {this.props.modalIsOpen && (
+          <DialogOverlay initialFocusRef={this.buttonRef} style={{ zIndex: 100 }}>
+            {this.state.isLoading ? (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  height: '685px',
+                  alignItems: 'center',
+                }}
+              >
+                <p>Loading... A spinner would probably be nice here</p>
+              </div>
+            ) : null}
+            <DialogContent>
+              <button
+                className="btn btn-subscribe"
+                ref={this.buttonRef}
+                onClick={this.props.closeModal}
+              >
+                close
+              </button>
+              <iframe
+                ref={this.iframeRef}
+                src="https://donorbox.org/embed/vetswhocode-donation?show_content=true"
+                style={{
+                  maxWidth: '100%',
+                  minWidth: '100%',
+                  maxHeight: 'none!important',
+                }}
+                seamless="seamless"
+                name="donorbox"
+                scrolling="yes"
+                allowpaymentrequest="true"
+                width="100%"
+                height="685px"
+                frameBorder={0}
+                onLoad={this.handleIframeLoaded}
+              />
+            </DialogContent>
+          </DialogOverlay>
+        )}
+      </>
+    )
+  }
 }
-
-// unused variable do we need this???
-
-// const closeButton = {
-//   right: '8px'
-// }
-
-const DonateModal = ({ modalIsOpen, closeModal }) => (
-  <Modal
-    style={container}
-    contentLabel="Donate"
-    isOpen={modalIsOpen}
-    onRequestClose={closeModal}
-    shouldCloseOnOverlayClick
-  >
-    <iframe
-      src="https://donorbox.org/embed/vetswhocode-donation?show_content=true"
-      style={{
-        maxWidth: '100%',
-        minWidth: '100%',
-        maxHeight: 'none!important',
-      }}
-      seamless="seamless"
-      name="donorbox"
-      scrolling="no"
-      allowpaymentrequest
-      width="100%"
-      height="685px"
-      frameBorder={0}
-    />
-  </Modal>
-)
 
 DonateModal.propTypes = {
   modalIsOpen: PropTypes.bool,
