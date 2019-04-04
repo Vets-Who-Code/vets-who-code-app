edeb9f43319e92423399749d233040dac3303a38
Andrew James <ajames20@gmail.com>
2019-04-04 17:42:31 -0500

    Add mail chimp config

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,12 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         purgeOnly : ['components/', '/main.css', 'bootstrap/', 'css/'], // Purge only these files/folders
       }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: 'https://vetswhocode.us12.list-manage.com/subscribe/post?u=80af3c15cfdb9ee5ad4bc6ee6&amp;id=642229d1fe',
+      },
     }
   ],
   pathPrefix: `/`
