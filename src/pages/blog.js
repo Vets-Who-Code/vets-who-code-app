80ece7c13d180e4b9975d7845741f3ba7e4bda3e
gixxerblade <sdclarkie@gmail.com>
2020-04-10 11:57:25 -0400

    Made cards for each blog post using grid. Added package named react-markdown to parse body_markdown from REST call to dev.to. Added some styling. Much more needs to be completed

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,35 +2,40 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
-
+import '../assets/css/blog.css'
+import '../assets/css/main.css'
 export const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allDevArticle.nodes
+  const stockImage =
+    'https://res.cloudinary.com/practicaldev/image/fetch/s--EXetaNc9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://res.cloudinary.com/practicaldev/image/fetch/s--7VrAA2ln--/c_imagga_scale%2Cf_auto%2Cfl_progressive%2Ch_420%2Cq_auto%2Cw_1000/https://thepracticaldev.s3.amazonaws.com/i/qmb5wkoeywj06pd7p8ku.png'
   return (
     <Layout>
       <PageHeader title="blog" />
-      {posts.map(node => {
-        const title = node.title || node.slug
-        return (
-          <article key={node.slug}>
-            <header>
-              <h3>
-                <Link style={{ boxShadow: `none` }} to={`posts/${node.id}`}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.published_at}</small>
-            </header>
-            <section>
+      <div id="blog-container">
+        {posts.map(node => {
+          const title = node.title || node.slug
+          return (
+            <div className="post-card" key={node.slug}>
+              {node.cover_image ? (
+                <img className="cover_image" src={node.cover_image} />
+              ) : (
+                <img className="cover_image" src={stockImage} />
+              )}
+              <h3 className="blog_title">{title}</h3>
+              <p className="date">{node.published_at}</p>
+              <p className="date">By {node.user.name}</p>
               <p
+                className="blog_description"
                 dangerouslySetInnerHTML={{
                   __html: node.description,
                 }}
               />
-            </section>
-          </article>
-        )
-      })}
+              <Link className='readmore' to={`posts/${node.id}`}>Read more...</Link>
+            </div>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
@@ -47,10 +52,16 @@ export const pageQuery = graphql`
     allDevArticle {
       nodes {
         id
+        cover_image
         title
         slug
         published_at(formatString: "MMMM DD, YYYY")
         description
+        positive_reactions_count
+        comments_count
+        user {
+          name
+        }
       }
     }
   }
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,22 +3,26 @@ import Layout from '../components/Layout'
 import PageHeader from '../components/PageHeader'
 import useFetch from '../hooks/useFetch'
 import { pageQuery } from './blog'
-import Img from 'gatsby-image'
-
+import ReactMarkdown from 'react-markdown'
+import '../assets/css/blog.css'
 const Posts = ({ location }) => {
   const query = location.pathname
   const id = query.replace(/\/posts\//, '')
   const { data, loading } = useFetch(`https://dev.to/api/articles/${id}`, {})
-  console.log(data.user)
+  console.log(data.body_markdown)
+  const md = data.body_markdown
   return (
     <Layout>
       <PageHeader title={data.title} />
-      <img src={data.cover_image} width="50%" />
-      <p>Author: {data.user.name}</p>
-      <p>Date: {data.readable_publish_date}</p>
-      <section>
-{/*       <div dangerouslySetInnerHTML={data.body_html}/>
- */}      </section>
+      <div className="container">
+        <img className="postimage" src={data.cover_image} width="50%" />
+        {/*       <p>Author: {data.user.name}</p>
+         */}{' '}
+        <p>Date: {data.readable_publish_date}</p>
+        <section>
+          <ReactMarkdown source={data.body_markdown} />
+        </section>
+      </div>
     </Layout>
   )
 }
