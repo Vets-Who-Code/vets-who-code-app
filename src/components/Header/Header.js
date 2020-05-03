e82793b17bc7ed7280e60a63931eb6dc07d50d74
Jerome Hardaway <jerome@vetswhocode.io>
2020-05-02 23:12:15 -0500

    added space

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,10 +43,10 @@ function Header() {
                   <Link to="/apply" className="btn btn-charity-default">
                     Apply
                   </Link>
-                                < Link className = "btn btn-charity-default"
-                                to = "/donate" >
-                                  Donate <
-                                  /Link>
+                  &nbsp;
+                  <Link to="/donate" className="btn btn-charity-default">
+                    Donate 
+                    </Link>
                 </div>
               </div>
             </div>
