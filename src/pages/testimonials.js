716a59a12daa7e7420ede2e367849c3ce80782d2
jerome.hardaway <jerome.hardaway@cbsinteractive.com>
2020-05-03 15:35:25 -0500

    add all testimonials to site

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -102,9 +102,9 @@ function Testimonial() {
                   />
                   <blockquote className="testimonial-text">
                     <p>
-                      &quot;#VetsWhoCode on paper is a web development boot camp.
-                      In my opinion it is the best transition assistance program out there.
-                      I am so grateful for the access to a new life the boot camp gave me.&quot;
+                      &quot;#VetsWhoCode's  tenacious focus on language and computer science fundamentals over frameworks was invaluable in my career transition from being a Radiology Technician in the US Army to building cloud scale infrastructure at Microsoft.
+                       The coaching from the #VetsWhoCode’s talented and passionate mentor network proved to be a priceless asset even after graduating.
+                        Come ready to learn and you will succeed here.&quot;
                       <br /> - Jeff Martin, US Army | DevOps Engineer, Microsoft/Github
                     </p>
                   </blockquote>
