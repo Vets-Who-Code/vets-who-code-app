058750e1984bbc0cedb8d69f9786fa56c117befe
gixxerblade <sdclarkie@gmail.com>
2020-10-01 15:56:45 -0400

    Added section to gatsby-ssr.js to change the <html> attribute. Added darl css variables. Added classes to some elements to change colors

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,8 @@
 const React = require('react')
 const Layout = require('./src/components/Layout').default
 
-exports.onRenderBody = ({ setPostBodyComponents }) => {
+exports.onRenderBody = ({ setPostBodyComponents, setPreBodyComponents }) => {
+  setPreBodyComponents(<MagicScriptTag />)
   setPostBodyComponents([
     <script key="https://linked.chat/web/a9LB63" src="https://linked.chat/web/a9LB63" defer />,
   ])
@@ -9,4 +10,14 @@ exports.onRenderBody = ({ setPostBodyComponents }) => {
 
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
+}
+
+const MagicScriptTag = () => {
+  let codeToRunOnClient = `
+  (function(){
+    const root = document.documentElement;
+    root.setAttribute("color-mode", "dark")
+  })()`
+  // eslint-disable-next-line react/no-danger
+  return <script key="magic-script-tag" dangerouslySetInnerHTML={{ __html: codeToRunOnClient }} />
 }
