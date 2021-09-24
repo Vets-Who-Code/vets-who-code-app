c624af0a83724a7455d66acb0bd1bb2e59650eaf
Jerome Hardaway <jerome@vetswhocode.io>
2021-09-24 16:10:55 -0400

    addded an apostrophe

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,7 +119,7 @@ while learning software skills that help them become gainfully employed in the t
             <div className="col-sm-6 event_content">
               <h3 className="next-cohort">Surprising Update</h3>
               <div className="event_excerpt">
-                <p>Launching something new Veterans Day. Be sure to apply!</p>
+                <p>Launching something new Veteran's Day. Be sure to apply!</p>
               </div>
             </div>
             <div className="col-sm-6 event_counter_container text-center">
