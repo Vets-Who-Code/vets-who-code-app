43f97b22e2c4d2561f6d359aff0ba1be84a30b40
Jerome Hardaway <jerome@vetswhocode.io>
2021-10-27 14:11:24 -0400

    linting

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -13,7 +13,10 @@ function Donate() {
             <div className="col-md-12 lead-in">
               <h1 className="story-title">Donate to #VetsWhoCode</h1>
               <p>
-                <i>Thank you for choosing to support #VetsWhoCode. Here we have a few ways to support our organization</i>
+                <i>
+                  Thank you for choosing to support #VetsWhoCode. Here we have a few ways to support
+                  our organization
+                </i>
               </p>
             </div>
             <div className="row"></div>
