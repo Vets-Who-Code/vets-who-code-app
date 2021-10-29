c4cff37d165b616d17545e8a238127f5a0016b61
Jerome Hardaway <jerome@vetswhocode.io>
2021-10-28 21:03:59 -0400

    Donor page donor box (#307)

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -13,7 +13,10 @@ function Donate() {
             <div className="col-md-12 lead-in">
               <h1 className="story-title">Donate to #VetsWhoCode</h1>
               <p>
-                <i>Help us continue to train more veterans.</i>
+                <i>
+                  Thank you for choosing to support #VetsWhoCode. Here we have a few ways to support
+                  our organization.
+                </i>
               </p>
             </div>
             <div className="row"></div>
@@ -24,17 +27,73 @@ function Donate() {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <div className="iframe-container embed-responsive">
-                <iframe
-                  title="#VWC Donorbox"
-                  src="https://donorbox.org/embed/vetswhocode-donation?show_content=true"
-                  seamless="seamless"
-                  name="donorbox"
-                  scrolling="yes"
-                  allowpaymentrequest="true"
-                  frameBorder={0}
-                  tabIndex={0}
+              <div className="story col-md-12">
+                <h2>Individual Contributions</h2>
+                <div className="iframe-container embed-responsive">
+                  <iframe
+                    title="#VWC Donorbox"
+                    src="https://donorbox.org/embed/vetswhocode-donation?show_content=true"
+                    seamless="seamless"
+                    name="donorbox"
+                    scrolling="yes"
+                    allowpaymentrequest="true"
+                    frameBorder={0}
+                    tabIndex={0}
+                  />
+                </div>
+              </div>
+              <div className="story col-sm-4">
+                <h3 className="text-center">Corporate Giving</h3>
+                <StaticImage
+                  src="../images/corporate-giving.jpeg"
+                  alt="Corporate Giving"
+                  width={1000}
+                  height={500}
+                  placeholder="blurred"
+                  layout="constrained"
                 />
+                <p>
+                  To ensure ease for those interested in supporting on the corporate level, we have
+                  created accounts on{' '}
+                  <a href="https://causes.benevity.org/causes/840-862122804">Benevity</a> and{' '}
+                  <a href="https://www.brightfunds.org/organizations/vets-who-code-inc">
+                    Brightfunds
+                  </a>{' '}
+                  to make it easy for your employees and company to donate to our cause.
+                </p>
+              </div>
+              <div className="story col-sm-4">
+                <h3 className="text-center">Github Sponsors</h3>
+                <StaticImage
+                  src="../images/github-sponsors.jpeg"
+                  alt="Github Sponsors"
+                  width={1000}
+                  height={500}
+                  placeholder="blurred"
+                  layout="constrained"
+                />
+                <p>
+                  We are an open source project and are a part of the Github Sponsors program. If
+                  you would like to sponsor us, please visit our{' '}
+                  <a href="https://github.com/sponsors/Vets-Who-Code">Github Sponsors page</a> to
+                  learn more.
+                </p>
+              </div>
+              <div className="story col-sm-4">
+                <h3 className="text-center">Amazon Smile</h3>
+                <StaticImage
+                  src="../images/amazon-smile.jpeg"
+                  alt="Amazon Smile"
+                  width={1000}
+                  height={500}
+                  placeholder="blurred"
+                  layout="constrained"
+                />
+                <p>
+                  Looking to give while shopping? We have a{' '}
+                  <a href="https://smile.amazon.com/ch/86-2122804">Smile</a> account to make it easy
+                  for you to give while shopping to your hearts content.
+                </p>
               </div>
             </div>
           </div>
@@ -50,7 +109,7 @@ function Donate() {
               <div className="cause_section_content">
                 <div className="testimonial-row">
                   <StaticImage
-                    src="../images/profile.png"
+                    src="../images/john-garcia.png"
                     alt="John Garcia"
                     style={{ height: 200, width: 200 }}
                   />
