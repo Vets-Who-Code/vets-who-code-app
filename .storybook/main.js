f42c07fb6a15b12b6f1ed312deed250fde6a5fff
Jerome Hardaway <jerome@vetswhocode.io>
2022-01-17 22:42:23 -0500

    added storybook npm modules

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-storysource',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
-}
+};
