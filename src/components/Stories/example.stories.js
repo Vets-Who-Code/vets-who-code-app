e5158e086001572edd5a011fc1a377ecc2891daf
Jerome Hardaway <jerome@vetswhocode.io>
2022-01-17 22:55:13 -0500

    create example story

---
+++ src/components/Stories/example.stories.js
@@ -0,0 +1,12 @@
+// example.stories.js
+
+import React from 'react';
+export default {
+  title: "Dashboard/header",
+}
+
+export const exampleStory = () => (
+  <div style={{padding: "16px", backgroundColor: "#eeeeee"}}>
+    <h1 style={{ color: "red" }}>Hello VetsWhoCode!!!</h1>
+  </div>
+)
