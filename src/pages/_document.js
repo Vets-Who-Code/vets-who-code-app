8e54aa0743eadef77c719c3efca69a841c5861a8
Jerome Hardaway <jerome@vetswhocode.io>
2022-04-04 22:18:17 -0500

    J hardaway add font (#352)

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,15 +10,7 @@ export default class CustomDocument extends Document {
     return (
       <>
         <Html lang="en" color-mode="light">
-          <Head>
-            <link rel="shortcut icon" href="/favicon.png" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Lato&family=Roboto+Slab&family=Roboto&display=swap"
-              rel="stylesheet"
-            />
-          </Head>
+          <Head />
           <body>
             <Main />
             <NextScript />
