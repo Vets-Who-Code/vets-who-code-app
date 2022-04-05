9b2c1361fd430bff53dd278f294e63831b25a926
Jerome Hardaway <jerome@vetswhocode.io>
2022-04-04 22:54:26 -0400

    remove google apis

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,12 +12,6 @@ export default class CustomDocument extends Document {
         <Html lang="en" color-mode="light">
           <Head>
             <link rel="shortcut icon" href="/favicon.png" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Lato&family=Roboto+Slab&family=Roboto&display=swap"
-              rel="stylesheet"
-            />
           </Head>
           <body>
             <Main />
