c296c2d71bcbe90a511481221b119c8438544194
Jerome Hardaway <jerome@vetswhocode.io>
2022-04-04 23:00:18 -0400

    remove head in imports

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Main, NextScript } from 'next/document'
 
 export default class CustomDocument extends Document {
   static async getInitialProps(ctx) {
