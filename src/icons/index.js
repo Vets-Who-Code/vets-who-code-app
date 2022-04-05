9530a361bff9db5231222b9e1975df88227bbed1
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-04-05 12:35:02 -0400

    updating brange from master

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,4 +1,4 @@
 export { default as Contentful } from './Contentful'
 export { default as Fem } from './Fem'
-export { default as Netlify } from './Netlify'
 export { default as Adzuna } from './Adzuna'
+export { default as Vercel } from './Vercel'
