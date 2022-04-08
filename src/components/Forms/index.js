227b9a4762ff25a2a079b0fd880cd6e02d045e48
Agohige-Tech <60114461+Agohige-Tech@users.noreply.github.com>
2022-04-07 20:18:36 -0400

    Added email and message box in footer

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -24,6 +24,7 @@ export const onSubmitError = message => {
 
 export { default as ApplyForm } from './ApplyForm'
 export { default as ContactForm } from './ContactForm'
+export { default as ContactFormFooter } from './ContactFormFooter'
 export { default as MentorForm } from './MentorForm'
 export { default as SubscribeForm } from './SubscribeForm'
 export { default as FormAlert } from './FormAlert'
