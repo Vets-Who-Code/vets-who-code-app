a22e117e41a3a0266b7509799beff2451a3f4362
Jerome Hardaway <jerome@vetswhocode.io>
2022-04-28 16:18:09 -0400

    added google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import ThemeProvider from '../store/ThemeProvider'
 import Nav from '@/components/Nav'
 import Footer from '@/components/Footer'
+import Script from 'next/script'
 
 // libraries
 import 'react-toastify/dist/ReactToastify.css'
@@ -57,6 +58,21 @@ export default function App({ Component, pageProps }) {
           <Footer />
         </main>
       </ThemeProvider>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
     </>
   )
 }
