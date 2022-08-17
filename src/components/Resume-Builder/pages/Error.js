ade9a7e871586805a8f31ca90412f4f5b4ea1746
Vanessa W <88523307+iNeso1984@users.noreply.github.com>
2022-08-17 18:20:14 -0400

    Router pages setup (#420)
    * create components file and added router
    
    * set up router and pages
    
    Co-authored-by: Vanessa W <73565294+Macrina1984@users.noreply.github.com>

---
+++ src/components/Resume-Builder/pages/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+const Error = () => {
+  const router = useRouter()
+  return (
+    <div>
+      <h1>Error! Page Not Found!</h1>
+      <button type="button" onClick={() => router.push('./Landing.js')}>
+        Go Back
+      </button>
+    </div>
+  )
+}
+
+export default Error
