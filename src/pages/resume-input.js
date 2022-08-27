ea1f823295d9138d4852f40d2d839d36789925cd
Ken <user@AurumMac.local>
2022-08-26 23:27:24 -0500

    removed error.js, added Link component and light styling to pages

--- src/pages/resume-input.js
+++ src/pages/resume-input.js
@@ -1,19 +1,20 @@
-import Link  from 'next/link'
+import Link from 'next/link'
 import { NextSeo } from 'next-seo'
 import PageHeader from '@/components/PageHeader'
-
 
 const ResumeInput = () => {
   return (
     <>
-    <NextSeo title='Resume Information'/>
-    <PageHeader />
-    <div className='container'>
-      <h1>Contact</h1>
-      <Link href="/confirmation" passHref>
-        <button className="btn-className">Next</button>
-</Link>
-    </div>
+      <NextSeo title="Resume Information" />
+      <PageHeader />
+      <div className="container">
+        <div class="resume-wrapper">
+          <h1>Contact</h1>
+          <Link href="/confirmation" passHref>
+            <button className="btn btn-gr">Next</button>
+          </Link>
+        </div>
+      </div>
     </>
   )
 }
