bd0965dc08bebdbfa07819efacb0aa901026acfe
Josh Morton <mountainmort@gmail.com>
2023-05-23 14:30:45 -0600

    feat: carousel paging dark mode

--- src/components/Testimonal/TestimonialCarousel.js
+++ src/components/Testimonal/TestimonialCarousel.js
@@ -2,6 +2,8 @@ import { getTestimonials } from '@/utilities/testimonials'
 import TestimonialCard from './TestimonialCard'
 import Carousel from 'nuka-carousel'
 import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
+import { useContext } from 'react'
+import { ThemeContext } from '@/store/ThemeProvider'
 
 const baseSettings = {
   autoplay: true,
@@ -13,25 +15,32 @@ const baseSettings = {
   height: '100%',
 }
 
-const previousSlide = ({ previousSlide }) => (
-  <button className="close" onClick={previousSlide}>
-    <BsArrowLeft size={30} />
-  </button>
-)
-
-const nextSlide = ({ nextSlide }) => (
-  <button className="close" onClick={nextSlide}>
-    <BsArrowRight size={30} />
-  </button>
-)
-
 function TestimonialCarousel() {
+  const { colorMode } = useContext(ThemeContext)
+
+  const previousSlide = ({ previousSlide }) => (
+    <button className="close" onClick={previousSlide}>
+      <BsArrowLeft size={30} fill={colorMode === 'dark' ? 'white' : 'black'} />
+    </button>
+  )
+
+  const nextSlide = ({ nextSlide }) => (
+    <button className="close" onClick={nextSlide}>
+      <BsArrowRight size={30} fill={colorMode === 'dark' ? 'white' : 'black'} />
+    </button>
+  )
+
   return (
     <>
       <Carousel
         {...baseSettings}
         renderCenterLeftControls={previousSlide}
         renderCenterRightControls={nextSlide}
+        defaultControlsConfig={{
+          pagingDotsStyle: {
+            fill: colorMode === 'dark' ? 'white' : 'black',
+          },
+        }}
       >
         {getTestimonials().map(testimonial => {
           return <TestimonialCard key={testimonial.id} testimonial={testimonial} />
