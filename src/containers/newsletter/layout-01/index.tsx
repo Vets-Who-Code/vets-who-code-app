f577cbb9a5d46cc75f4bb5a2e561e11b8a3ff825
Jerome Hardaway <jhardaway@microsoft.com>
2023-06-27 19:48:29 -0400

    fixing errors

--- src/containers/newsletter/layout-01/index.tsx
+++ src/containers/newsletter/layout-01/index.tsx
@@ -34,10 +34,7 @@ const NewsletterArea: React.FC<TProps> = ({
                 variants={scrollUpVariants}
             >
                 {section_title && <SectionTitle {...section_title} />}
-                <NewsletterForm
-                    className="tw-mx-auto tw-mt-[50px]"
-                    onSubmit={handleSubmit}
-                />
+                <NewsletterForm onSubmit={handleSubmit} />
 
                 <motion.div
                     className="tw-absolute tw-bottom-[50px] tw-left-[-3px] md:tw-left-0 tw-z-20"
@@ -49,19 +46,6 @@ const NewsletterArea: React.FC<TProps> = ({
                     <span className="tw-block -tw-indent-[99999px] tw-border-[6px] tw-border-primary-light tw-rounded-full tw-w-[45px] tw-h-[45px] md:tw-border-[7px] md:tw-w-14 md:tw-h-14">
                         shape 1
                     </span>
-                </motion.div>
-
-                <motion.div
-                    className="tw-absolute -tw-z-1 -tw-bottom-[45px] tw-right-0 tw-top-7.5 tw-w-[100px] md:tw-w-auto md:tw-right-2.5"
-                    animate={{
-                        x: trans1().x,
-                        y: trans1().y,
-                    }}
-                >
-                    <img
-                        src="/images/shape-animation/nwesletter-shape-1.png"
-                        alt=""
-                    />
                 </motion.div>
             </motion.div>
         </Section>
