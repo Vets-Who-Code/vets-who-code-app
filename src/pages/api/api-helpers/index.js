1ff707cedc5288095a72ddfa6fa771af633b0b6f
Jerome Hardaway <jhardaway@microsoft.com>
2023-06-27 20:22:41 -0400

    add subscribe.js

---
+++ src/pages/api/api-helpers/index.js
@@ -0,0 +1,24 @@
+export function checkParams(eventBody, params) {
+    let hasError = false;
+
+    for (let k of params) {
+        if (typeof eventBody[k] === "undefined" || !eventBody[k]) {
+            hasError = true;
+        }
+    }
+
+    return hasError;
+}
+
+export const checkLength = (message) => {
+    const length = message.trim().split(" ").length;
+    if (length === 1) {
+        return true;
+    }
+    return false;
+};
+
+export const contactErrors = {
+    missingOrRequired: "Missing or incorrect required property",
+    tooShort: "Message is too short for submission",
+};
