3525d1cc9e66f981f1c280a71bb97d26b7adad40
Jerome Hardaway <jhardaway@microsoft.com>
2023-07-04 17:31:16 -0400

    save

--- src/pages/code-of-conduct.tsx
+++ src/pages/code-of-conduct.tsx
@@ -5,6 +5,12 @@ import Breadcrumb from "@components/breadcrumb";
 import MarkdownRenderer from "@components/markdown-renderer";
 import { getPageBySlug } from "../lib/mdx-pages";
 
+type TProps = {
+    data: {
+        page: string;
+    };
+};
+
 type PageProps = NextPage<TProps> & {
     Layout: typeof Layout;
 };
@@ -17,7 +23,7 @@ const CodeOfConduct: PageProps = ({ data }) => {
                 pages={[{ path: "/", label: "home" }]}
                 currentPage="Code Of Conduct"
             />
-            <div className="tw-container tw-pb-15 md:tw-pb-20 lg:tw-pb-[100px] w-full tw-gap-7.5 lg:tw-gap-15">
+            <div className="tw-container tw-pb-15 md:tw-pb-20 lg:tw-pb-[100px] tw-grid tw-grid-cols-3 tw-gap-7.5 lg:tw-gap-15">
                 <div className="tw-col-span-full tw-order-1 lg:tw-order-2 lg:tw-col-[2/-1]">
                     <MarkdownRenderer content={data.page} />
                 </div>
@@ -30,11 +36,13 @@ CodeOfConduct.Layout = Layout;
 
 export const getStaticProps: GetStaticProps = () => {
     const page = getPageBySlug("code-of-conduct");
+    const recentCourses = getallCourses(["title", "thumbnail"], 0, 4);
 
     return {
         props: {
             data: {
                 page,
+                recentCourses,
             },
             layout: {
                 headerShadow: true,
