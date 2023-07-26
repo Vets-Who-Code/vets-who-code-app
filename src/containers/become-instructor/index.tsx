6bea2021b41029f01aa41191433c3c680f941ef2
Jerome Hardaway <jhardaway@microsoft.com>
2023-07-26 17:22:37 -0400

    changed text; added classes in another div

--- src/containers/become-instructor/index.tsx
+++ src/containers/become-instructor/index.tsx
@@ -1,11 +1,20 @@
+// File: BecomeInstructor.tsx
+
+import React from "react";
 import InstructorForm from "@components/forms/mentor-form";
 
-const BecomeInstructor = () => {
+interface BecomeInstructorProps {
+    className?: string; // Add a className prop to the BecomeInstructor component
+}
+
+const BecomeInstructor: React.FC<BecomeInstructorProps> = ({ className }) => {
     return (
-        <div className="tw-py-15 md:tw-py-20 lg:tw-py-[100px] tw-bg-[url('/images/bg/become-a-teache-bg.jpg')] tw-bg-cover tw-bg-fixed">
+        <div
+            className={`tw-py-15 md:tw-py-20 lg:tw-py-[100px] tw-bg-[url('/images/bg/become-a-teache-bg.jpg')] tw-bg-cover tw-bg-fixed ${className}`}
+        >
             <div className="tw-container">
                 <div className="tw-flex tw-justify-center">
-                    <InstructorForm className="lg:tw-w-2/3 xl:tw-w-1/2 lg:tw-ml-auto" />
+                    <InstructorForm />
                 </div>
             </div>
         </div>
