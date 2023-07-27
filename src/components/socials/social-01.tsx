794f02d2b1b2bf88ce1cd9f2c4d5076d6ab6d64a
Jerome Hardaway <jhardaway@microsoft.com>
2023-07-27 00:43:57 -0400

    cleaned up contact page

--- src/components/socials/social-01.tsx
+++ src/components/socials/social-01.tsx
@@ -4,28 +4,28 @@ import Social, { SocialLink } from "@ui/social";
 const Social01 = ({ className }: { className?: string }) => (
     <Social color="dark" size="lg" className={clsx(className)}>
         <SocialLink
-            href="https://www.twitter.com/"
-            label="twitter"
+            href="https://github.com/Vets-Who-Code"
+            label="github"
             className="tw-px-2.5"
         >
-            <i className="fab fa-twitter" />
+            <i className="fab fa-github" />
         </SocialLink>
         <SocialLink
-            href="https://www.facebook.com/"
+            href="https://www.facebook.com/TheOfficialVetsWhoCode/"
             label="facebook"
             className="tw-px-2.5"
         >
             <i className="fab fa-facebook-f" />
         </SocialLink>
         <SocialLink
-            href="https://www.instagram.com/"
-            label="instagram"
+            href="https://dev.to/vetswhocode"
+            label="Practical Dev"
             className="tw-px-2.5"
         >
-            <i className="fab fa-instagram" />
+            <i className="fab fa-dev" />
         </SocialLink>
         <SocialLink
-            href="https://www.linkedin.com/"
+            href="https://www.linkedin.com/company/vets-who-code"
             label="linkedin"
             className="tw-px-2.5"
         >
