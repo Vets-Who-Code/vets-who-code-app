0463a4d395fe521200eaa142a0df66de64edcb44
angelospk <55174356+angelospk@users.noreply.github.com>
2024-01-19 17:37:35 +0200

    added shop button on menu.ts file and changed tw-px to 15 px (#540)

--- src/data/sidebar-menu.ts
+++ src/data/sidebar-menu.ts
@@ -1,7 +1,12 @@
 export default [
     { id: 1, path: "/about-us", label: "About Us" },
     { id: 2, path: "/contact-us", label: "Contact Us" },
-    { id: 3, path: "/apply", label: "Apply" },
-    { id: 4, path: "/mentor", label: "Mentor" },
-    { id: 5, path: "/code-of-conduct", label: "Code Of Conduct" },
+    {
+        id: 3,
+        label: "Shop",
+        path: "https://hashflag.shop/",
+    },
+    { id: 4, path: "/apply", label: "Apply" },
+    { id: 5, path: "/mentor", label: "Mentor" },
+    { id: 6, path: "/code-of-conduct", label: "Code Of Conduct" },
 ];
