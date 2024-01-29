04597a8dbf6e50fe82a336306188a4719f587e14
Kenneth Brandon <48884692+kbrandon19@users.noreply.github.com>
2024-01-29 15:53:30 -0500

    Updated footer to include Platinum Transparency Award.  (#543)
    * Updated footer to include Platinum Transparency Award, ensuring size is maintained at each breakpoint
    
    * Removed the physical and replaced with cloudinary link, updated alt text

--- src/layouts/footers/footer-01.tsx
+++ src/layouts/footers/footer-01.tsx
@@ -21,16 +21,24 @@ const Footer01 = ({ mode }: TProps) => {
                 <div className="tw-grid tw-grid-cols-12">
                     <TextWidget
                         mode={mode}
-                        className="tw-col-span-12 lg:tw-col-span-5 xl:tw-col-span-6 tw-mb-[43px]"
+                        className="tw-col-span-12 lg:tw-col-span-5 xl:tw-col-span-5 tw-mb-[43px]"
                     />
                     <TwoColumnListWidget
                         mode={mode}
                         className="tw-col-span-12 md:tw-col-span-7 lg:tw-col-span-4 xl:tw-col-span-3 tw-mb-[25px]"
                     />
+                    
                     <ListWidget
                         mode={mode}
                         className="tw-col-span-12 md:tw-col-span-5 lg:tw-col-span-3 tw-mb-7.5"
                     />
+                    <div className="maxSm:tw-col-span-3 sm:tw-col-span-2 xl:tw-col-span-1">
+                    <img 
+                                src="https://res.cloudinary.com/vetswhocode/image/upload/v1705549609/candid-seal-platinum-2024_wexkdk.png"
+                                alt="Candid Seal Platinum Award"
+                                width={150}
+                            />
+                    </div>
                 </div>
                 <p className="copyright tw-text-center tw-text-md tw-text-gray-400 tw-mt-5">
                     &copy; {new Date().getFullYear()} Vets Who Code{" "}
