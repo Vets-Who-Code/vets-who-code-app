c87829a86a90a8cfde5b82030801610fba83d776
Jerome Hardaway <jerome@vetswhocode.io>
2024-03-17 01:35:41 -0400

    added initial tooling (#570)
    * added initial tooling
    
    * fix .gitignore
    
    * fix the _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,27 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
     render() {
         const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
+        const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID; // Get the GA ID from environment variables
 
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    {gaId && ( // Only include the script if the GA ID is present
+                        <>
+                            <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} /> {/* Make self-closing */}
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                        window.dataLayer = window.dataLayer || [];
+                                        function gtag(){dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+                                        gtag('config', '${gaId}');
+                                    `,
+                                }}
+                            /> {/* Make self-closing */}
+                        </>
+                    )}
+                </Head>
                 <body className={pageProps.className}>
                     <Main />
                     <div id="portal" />
