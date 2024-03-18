695ea65ac2d61dd32ac0247206fa0a5932ab1cea
Cameron <cporter@us.ci.org>
2024-03-18 09:37:34 -0600

    updated static props and removed unused code

--- src/pages/medias/media/page/[page].tsx
+++ src/pages/medias/media/page/[page].tsx
@@ -8,86 +8,84 @@ import { IMedia } from "@utils/types";
 import { getAllMedia } from "../../../../lib/media";
 
 type TProps = {
-  data: {
-      medias: IMedia[];
-  };
+    data: {
+        medias: IMedia[];
+    };
 };
 
 type PageProps = NextPage<TProps> & {
-  Layout: typeof Layout01;
+    Layout: typeof Layout01;
 };
 
 const POSTS_PER_PAGE = 9;
 
-const MediaGrid: PageProps = ({
-  data: { medias}
-}) => {
-  return (
-      <>
-          <SEO title={`Media Grid - Page - 1`} />
-          <Breadcrumb
-              pages={[{ path: "/", label: "home" }]}
-              currentPage="Blog Grid"
-          />
-          <MediaArea
-              data={{
-                  medias
-              }}
-          />
-      </>
-  );
+const MediaGrid: PageProps = ({ data: { medias } }) => {
+    return (
+        <>
+            <SEO title={`Media Grid - Page - 1`} />
+            <Breadcrumb
+                pages={[{ path: "/", label: "home" }]}
+                currentPage="Blog Grid"
+            />
+            <MediaArea
+                data={{
+                    medias,
+                }}
+            />
+        </>
+    );
 };
 
 MediaGrid.Layout = Layout01;
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const { count } = getAllMedia([]);
-  const pages = Math.ceil(count / POSTS_PER_PAGE);
+    const { count } = getAllMedia([]);
+    const pages = Math.ceil(count / POSTS_PER_PAGE);
 
-  const pagesToGenerate = [...Array(pages).keys()]
-      .map((a) => {
-          if (a !== 0) return a + 1;
-          return null;
-      })
-      .filter(Boolean);
+    const pagesToGenerate = [...Array(pages).keys()]
+        .map((a) => {
+            if (a !== 0) return a + 1;
+            return null;
+        })
+        .filter(Boolean);
 
-  const paths = pagesToGenerate.map((page) => {
-      return { params: { page: String(page) } }; // cast page to string
-  });
-  return {
-      paths,
-      fallback: false,
-  };
+    const paths = pagesToGenerate.map((page) => {
+        return { params: { page: String(page) } }; // cast page to string
+    });
+    return {
+        paths,
+        fallback: false,
+    };
 };
 
 interface Params extends ParsedUrlQuery {
-  page: string;
+    page: string;
 }
 
 export const getStaticProps: GetStaticProps<TProps, Params> = ({ params }) => {
-  const page = params?.page;
-  const currentPage = !page || Number.isNaN(+page) ? 1 : +page;
-  const skip = (currentPage - 1) * POSTS_PER_PAGE;
-  const { medias, count } = getAllMedia(
-      ["title", "slug", "image", "category", "postedAt", "views"],
-      skip,
-      POSTS_PER_PAGE
-  );
+    const page = params?.page;
+    const currentPage = !page || Number.isNaN(+page) ? 1 : +page;
+    const skip = (currentPage - 1) * POSTS_PER_PAGE;
+    const { medias, count } = getAllMedia(
+        ["title", "slug", "image", "datePublished"],
+        skip,
+        POSTS_PER_PAGE
+    );
 
-  return {
-      props: {
-          data: {
-              medias,
-              currentPage,
-              numberOfPages: Math.ceil(count / POSTS_PER_PAGE),
-          },
-          layout: {
-              headerShadow: true,
-              headerFluid: false,
-              footerMode: "light",
-          },
-      },
-  };
+    return {
+        props: {
+            data: {
+                medias,
+                currentPage,
+                numberOfPages: Math.ceil(count / POSTS_PER_PAGE),
+            },
+            layout: {
+                headerShadow: true,
+                headerFluid: false,
+                footerMode: "light",
+            },
+        },
+    };
 };
 
 export default MediaGrid;
