c625946cf92d2fd01f1ed9c21ac00f75c89aabee
Jerome Hardaway <jerome@vetswhocode.io>
2024-06-09 00:20:14 -0400

    save

--- src/components/seo/page-seo.tsx
+++ src/components/seo/page-seo.tsx
@@ -58,8 +58,8 @@ const PageSeo = ({
                 title={title}
                 titleTemplate={
                     template
-                        ? `${title} - ${template}`
-                        : siteConfig.titleTemplate
+                        ? `${title ?? ""} - ${template}`
+                        : `%s - ${siteConfig.titleTemplate}`
                 }
                 description={description}
                 openGraph={{
