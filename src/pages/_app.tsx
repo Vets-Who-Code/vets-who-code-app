f30e947bbdf11442313c0ddf37b11dae81b91b18
Jerome Hardaway <jerome@vetswhocode.io>
2024-10-18 23:27:25 -0400

    use it like this

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics';
 import { ElementType, useEffect } from "react";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
