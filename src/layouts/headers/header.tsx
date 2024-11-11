66356c2d9b3b69431bae0569edb5a6a979cbd1a0
Jerome Hardaway <jerome@vetswhocode.io>
2024-11-11 05:15:24 -0500

    update the cohort timer (#632)

--- src/layouts/headers/header.tsx
+++ src/layouts/headers/header.tsx
@@ -35,11 +35,11 @@ const Header = ({ shadow, fluid }: TProps) => {
                 <div className="header-top tw-py-2.5 tw-bg-gray-200">
                     <div className="tw-container tw-flex tw-justify-center tw-items-center tw-flex-wrap">
                         <p className="tw-text-center tw-flex-100 tw-mb-3.8 md:tw-flex-1 md:tw-text-left md:tw-mb-0 md:tw-mr-7.5">
-                            Our 10th Anniversary Is In
+                            Our Next Cohort
                         </p>
                         <div className="tw-flex tw-items-center sm:tw-mr-[45px] md:tw-mr-5 lg:tw-mr-[45px]">
                             <i className="far fa-clock tw-text-lg tw-text-secondary tw-mr-[5px]" />
-                            <CountdownTimer targetDate="2024/05/15" />
+                            <CountdownTimer targetDate="2025/03/13" />
                         </div>
                         <Button size="xs" path="/donate">
                             Donate
