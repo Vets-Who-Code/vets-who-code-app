895fd8eadff89e75008ab70a36747bebbc9e8cd7
Sherika <faysons81@outlook.com>
2024-12-18 15:45:21 -0500

    Work in progress for shopify ui

---
+++ src/utils/shopify.js
@@ -0,0 +1,40 @@
+export async function shopifyFetch({ query, variables }) {
+    const endpoint = `https://${process.env.SHOPIFY_STORE_DOMAIN}/api/2023-10/graphql.json`;
+    const key = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN;
+
+    if (!endpoint == !key) {
+        throw new Error("Shopify Store domain or access token is missing.");
+    }
+
+    const body = JSON.stringify({ query, variables });
+
+    try {
+        const result = await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Shopify-Storefront-Access-Token': key,
+            },
+            body,
+        });
+
+        if (!result.ok) {
+            const errorDetails = await result.text();
+            throw new Error(`Shopify API error: ${result.status} - ${errorDetails}`);
+        }
+
+        const responseBody = await result.json();
+
+        return {
+            status: result.status,
+            body: responseBody,
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+          status: 500,
+          error: error.message || 'Error receiving data',
+        };
+    }
+    
+}
