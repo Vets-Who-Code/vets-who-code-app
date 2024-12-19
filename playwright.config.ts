7b4deac3593d4e3f9f41c404320ff4f62f2cf1d1
Jerome Hardaway <jerome@vetswhocode.io>
2024-12-18 20:17:17 -0500

    add to playwright to depenencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 
+dotenv.config();
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +25,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
+        baseURL: "http://localhost:3000",
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
@@ -42,33 +43,27 @@ export default defineConfig({
             use: { ...devices["Desktop Firefox"] },
         },
 
-        /* Removed WebKit project */
-
         /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
+        {
+            name: "Mobile Chrome",
+            use: { ...devices["Pixel 5"] },
+        },
 
         /* Test against branded browsers. */
-        // {
-        //   name: 'Microsoft Edge',
-        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-        // },
-        // {
-        //   name: 'Google Chrome',
-        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-        // },
+        {
+            name: "Microsoft Edge",
+            use: { ...devices["Desktop Edge"], channel: "msedge" },
+        },
+        {
+            name: "Google Chrome",
+            use: { ...devices["Desktop Chrome"], channel: "chrome" },
+        },
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+        command: "npm run start",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
+    },
 });
