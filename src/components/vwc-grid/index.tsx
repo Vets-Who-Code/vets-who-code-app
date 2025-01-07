6a7360dd25c7241dcb8124e1b23293d3d2d6fe19
jonulak <jonulak@gmail.com>
2025-01-06 19:00:36 -0500

    Revert class name change in VWCGrid

--- src/components/vwc-grid/index.tsx
+++ src/components/vwc-grid/index.tsx
@@ -9,7 +9,7 @@ export const VWCGrid = ({ title, children }: PropsWithChildren<TProps>) => {
     // const { itemsToShow } = useLoadMore<ICourse>(sortedItems, 9, 3);
 
     return (
-        <Section className={`${title}-area`} space="bottom">
+        <Section className="course-area" space="bottom">
             <h2 className="tw-sr-only">{title}</h2>
             <div className="tw-container">
                 <div className="tw-grid tw-grid-cols-1 tw-gap-[30px] md:tw-grid-cols-2 lg:tw-grid-cols-3">
