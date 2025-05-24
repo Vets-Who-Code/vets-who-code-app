53b32ad151ac0d87e2e93b4f188bedf31b4232c2
Jerome Hardaway <jerome@vetswhocode.io>
2025-05-23 23:04:37 -0400

    updating donor form

--- src/components/forms/donate-form.tsx
+++ src/components/forms/donate-form.tsx
@@ -14,8 +14,10 @@ const DonateForm = forwardRef<HTMLDivElement, TProps>(({ className }, ref) => {
             {/* Donation form column */}
             <div className="tw-w-full lg:tw-w-full">
                 <div className="tw-flex tw-flex-col tw-overflow-hidden tw-rounded-lg tw-bg-white tw-shadow-lg md:tw-flex-row">
-                    <div className="tw-bg-primary tw-p-3 tw-text-white md:tw-w-1/4">
-                        <h3 className="tw-text-xl tw-font-bold">Make a Difference Today</h3>
+                    <div className="tw-bg-primary tw-p-3 tw-text-white lg:tw-w-1/4">
+                        <h3 className="tw-text-xl tw-font-bold tw-text-white">
+                            Make a Difference Today
+                        </h3>
                         <p className="tw-mt-1 tw-text-sm tw-text-gray-100">
                             Your donation directly supports veterans learning to code
                         </p>
@@ -56,14 +58,10 @@ const DonateForm = forwardRef<HTMLDivElement, TProps>(({ className }, ref) => {
                         </div>
 
                         <div className="tw-mt-2 tw-flex tw-items-center tw-justify-between tw-text-xs">
-                            <svg
-                                className="tw-h-4 tw-w-4 tw-text-white"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
+                            <a
+                                href="mailto:info@vetswhocode.io"
+                                className="tw-text-white hover:tw-underline"
                             >
-                                <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
-                            </svg>
-                            <a href="#" className="tw-text-white hover:tw-underline">
                                 Contact us
                             </a>
                         </div>
@@ -75,7 +73,7 @@ const DonateForm = forwardRef<HTMLDivElement, TProps>(({ className }, ref) => {
                             src="https://donorbox.org/embed/vetswhocode-donation?show_content=true&default_interval=o&amount=25&hide_donation_meter=true&compact=true"
                             name="vwc-donorbox"
                             seamless
-                            className="tw-h-[400px] tw-w-full tw-border-none"
+                            className="tw-h-[950px] tw-w-full tw-border-none"
                         />
                     </div>
                 </div>
