8ebaf53038fdb1466a56007b5795c35c8c1744e3
jonulak <jonulak@gmail.com>
2025-07-23 19:52:13 -0400

    Update chat API route max duration to 300s

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth/next";
 import { NextRequest, NextResponse } from "next/server";
 import { options } from "@/pages/api/auth/options";
 
-export const maxDuration = 45; // Extend max duration for route on vercel to 45 seconds
+export const maxDuration = 300; // Extend max duration for route on vercel to 45 seconds
 
 export async function POST(req: NextRequest) {
     const session = await getServerSession(options);
@@ -12,10 +12,6 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ error: "You must be logged in to access this API route." });
     }
     const { messages }: { messages: CoreMessage[] } = await req.json();
-
-    messages.forEach((msg) => {
-        console.log(msg.content);
-    });
 
     const response = gemini.chat(messages);
     return response.toDataStreamResponse();
