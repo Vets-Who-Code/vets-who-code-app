020843c3064bf78a5be7ea154fac628c4d711990
jeromehardaway <jerome@vetswhocode.io>
2025-10-28 23:35:36 -0400

    fix form

--- src/components/forms/apply-form.tsx
+++ src/components/forms/apply-form.tsx
@@ -61,7 +61,7 @@ const ApplyForm = () => {
 
     const nextStep = async () => {
         const currentFields = STEPS[currentStep - 1].fields;
-        const isValid = await trigger(currentFields as (keyof IFormValues)[]);
+        const isValid = await trigger(currentFields as unknown as (keyof IFormValues)[]);
 
         if (isValid && currentStep < STEPS.length) {
             setCurrentStep(currentStep + 1);
