8f0292182b3922f6597ea6bd8ad41d06fc476c0c
jinu2ID <jinu.jacob90@gmail.com>
2026-05-09 16:32:52 -0400

    chore: Configure Storybook preview with global styles (#914)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,13 @@
-import type { Preview } from '@storybook/nextjs'
+import type { Preview } from "@storybook/nextjs";
+// @ts-ignore: side-effect import of CSS globals
+import "../src/assets/css/globals.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
