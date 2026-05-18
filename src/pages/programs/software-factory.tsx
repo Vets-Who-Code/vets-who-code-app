5933bf417f405c82927325185d633704d75e4f36
jeromehardaway <jerome@vetswhocode.io>
2026-05-17 23:23:38 -0400

    feat(software-factory): Remove testimonials section
    Drops the testimonials block from the Software Factory page and deletes
    the now-unused container. The page leads with capabilities, process,
    engagements, sponsorship, team, and differentiators into the CTA without
    relying on social proof we cannot yet substantiate at the engagement
    tier.

--- src/pages/programs/software-factory.tsx
+++ src/pages/programs/software-factory.tsx
@@ -7,7 +7,6 @@ import HeroSection from "@containers/software-factory/hero";
 import ProcessSection from "@containers/software-factory/process";
 import SponsorSection from "@containers/software-factory/sponsor";
 import TeamSection from "@containers/software-factory/team";
-import TestimonialsSection from "@containers/software-factory/testimonials";
 import Layout from "@layout/layout-01";
 import { NextPage } from "next";
 
@@ -25,7 +24,6 @@ const SoftwareFactoryPage: NextPage & { Layout: typeof Layout } = () => {
             <SponsorSection />
             <TeamSection />
             <DifferentiatorsSection />
-            <TestimonialsSection />
             <CtaSection />
         </>
     );
